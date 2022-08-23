fa7a2f0cfa1cade4d3ceb0ff753ddf24969a10fc
tungdvtownsoft <111406405+tungdvtownsoft@users.noreply.github.com>
2022-08-23 20:04:46 +0700

    update menu

--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -20,7 +20,7 @@
       </div>
     </a-layout-header>
     <a-layout>
-      <a-layout-sider width="250" >
+      <a-layout-sider width="300" >
         <a-menu
       
         mode="inline"
@@ -113,6 +113,150 @@ export default defineComponent({
               {name: '기초정보설정', url: '#'},
               {name: '회계설정', url: '#'},
               {name: '원천설정', url: '#'}
+             ]
+          },
+        ]
+      },
+      {
+        title: '회계모듈',
+        subMenus: [
+          {
+            title: '입력/마감',
+            items: [
+              {name: '통장내역', url: '#'},
+              {name: '전표', url: '#'},
+              {name: '마감', url: '#'}
+             ]
+          },
+          {
+            title: 'W4C',
+            items: [
+              {name: '전표/결의', url: '#'},
+              {name: '예산서', url: '#'},
+              {name: '결산서', url: '#'}
+             ]
+          },
+          {
+            title: '예산',
+            items: [
+              {name: '세입예산서', url: '#'},
+              {name: '세출예산서', url: '#'},
+              {name: '예산총괄표', url: '#'},
+              {name: '임직원보수일람표', url: '#'},
+              {name: '과목전용조서', url: '#'},
+              {name: '추가경정 세입예산서', url: '#'},
+              {name: '추가경정 세출예산서', url: '#'},
+              {name: '예산서 자동작성', url: '#'},
+             ]
+          },
+           {
+            title: '결산',
+            items: [
+              {name: '세입결산서', url: '#'},
+              {name: '세출결산서', url: '#'},
+              {name: '예비비사용조서', url: '#'},
+              {name: '정부보조금명세서', url: '#'},
+              {name: '후원금수입및사용결고보고서', url: '#'},
+              {name: '후원금계좌거래내역', url: '#'},
+              {name: '사업비명세서', url: '#'},
+              {name: '기타비용명세서', url: '#'},
+             ]
+          },
+          {
+            title: '보고서',
+            items: [
+              {name: '장부', url: '#'},
+              {name: '필요경비대장', url: '#'},
+              {name: '정부보조금명세서', url: '#'},
+              {name: '보조금정산서', url: '#'},
+             ]
+          },
+        ]
+      },
+      {
+        title: '원천모듈',
+        subMenus: [
+          {
+            title: '근로소득관리',
+            items: [
+              {name: '근로소득자료입력', url: '#'},
+              {name: '사원등록', url: '#'},
+             ]
+          },
+          {
+            title: '세무신고,연말정산',
+            items: [
+              {name: '원천징수이행상황신고서', url: '#'},
+              {name: '근로소득원천징수영수증', url: '#'},
+              {name: '소득자별근로소득원천징수부', url: '#'},
+              {name: '연말정산간소화입력', url: '#'},
+              {name: '의료비지급명세서', url: '#'},
+              {name: '기부금명세서', url: '#'},
+              {name: '연말정산현황', url: '#'}
+             ]
+          },
+          {
+            title: '전자신고',
+            items: [
+              {name: '원천세,지방소득세신고', url: '#'},
+              {name: '지급명세서전자신고(연말정산)', url: '#'},
+              {name: '간이지급명세서전자신고', url: '#'},
+              {name: '일용직전자신고', url: '#'},
+             ]
+          },
+           {
+            title: '퇴직소득관리',
+            items: [
+              {name: '퇴직금시뮬레이션', url: '#'},
+              {name: '퇴직소득자료입력', url: '#'},
+              {name: '퇴직소득원천징수영수증', url: '#'},
+             ]
+          },
+          {
+            title: '일용직근로소득관리',
+            items: [
+              {name: '일용직근로소득자료입력', url: '#'},
+              {name: '일용직사원등록', url: '#'},
+              {name: '일용직근로소득원천징수영수증', url: '#'},
+             ]
+          },
+          {
+            title: '사업소득관리',
+            items: [
+              {name: '사업소득자등록', url: '#'},
+              {name: '사업소득자료입력', url: '#'},
+              {name: '사업소득원천징수영수증', url: '#'},
+             ]
+          },
+          {
+            title: '기타소득관리',
+            items: [
+              {name: '기타소득자등록', url: '#'},
+              {name: '기타소득자등록', url: '#'},
+              {name: '기타소득자료입력', url: '#'},
+              {name: '기타소득원천징수영수증', url: '#'},
+             ]
+          },
+          {
+            title: '4대보험',
+            items: [
+              {name: '취득신고', url: '#'},
+              {name: '상실신고', url: '#'},
+              {name: '급여변경신고', url: '#'},
+              {name: '휴직복직신고', url: '#'},
+              {name: '보수총액신고', url: '#'},
+              {name: '보험사무대행신고', url: '#'},
+              {name: '사업장가입신고', url: '#'},
+              {name: '사업장탈퇴신고', url: '#'},
+              {name: '통합업무조회', url: '#'},
+             ]
+          },
+          {
+            title: '데이터연동',
+            items: [
+              {name: '4대보험EDI 데이터연동', url: '#'},
+              {name: '파트너십 계좌통합 데이터연동', url: '#'},
+              {name: '뱅크다K 데이터연동', url: '#'},
              ]
           },
         ]
