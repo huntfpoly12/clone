82fb96518018c6911bacb1431faa4f22b639b795
tungdvtownsoft <111406405+tungdvtownsoft@users.noreply.github.com>
2022-08-28 16:19:41 +0700

    update datagrid

--- src/views/BF/BF3/BF310.vue
+++ src/views/BF/BF3/BF310.vue
@@ -1,6 +1,127 @@
 <template>
-    <div class="bf-310">
-        <h2>계약정보관리,심사</h2>
+    <div id="data-grid-demo">
+      <DxDataGrid
+        :data-source="dataSource"
+        :show-borders="true"
+        key-expr="ID"
+      >
+      <DxSelection mode="multiple"/>
+        <DxPaging :enabled="false"/>
+        <DxEditing
+          :allow-updating="true"
+          :allow-adding="true"
+          :allow-deleting="false"
+          mode="popup"
+        >
+          <DxPopup
+            :show-title="true"
+            :width="700"
+            :height="525"
+            title="Employee Info"
+          />
+          <DxForm>
+            <DxItem
+              :col-count="2"
+              :col-span="2"
+              item-type="group"
+            >
+              <DxItem data-field="FirstName"/>
+              <DxItem data-field="LastName"/>
+              <DxItem data-field="Prefix"/>
+              <DxItem data-field="BirthDate"/>
+              <DxItem data-field="Position"/>
+              <DxItem data-field="HireDate"/>
+              <DxItem
+                :col-span="2"
+                :editor-options="{ height: 100 }"
+                data-field="Notes"
+                editor-type="dxTextArea"
+              />
+            </DxItem>
+  
+            <DxItem
+              :col-count="2"
+              :col-span="2"
+              item-type="group"
+              caption="Home Address"
+            >
+              <DxItem data-field="StateID"/>
+              <DxItem data-field="Address"/>
+            </DxItem>
+          </DxForm>
+        </DxEditing>
+        <DxSearchPanel
+            :visible="true"
+            :highlight-case-sensitive="true"
+            />
+        <DxColumn data-field="신청일자"/>
+        <DxColumn data-field="신청코드"/>
+        <DxColumn
+          data-field="심사상태"
+          data-type="date"
+        />
+        <DxColumn
+          :width="170"
+          data-field="사업자코드"
+        />
+        <DxColumn
+          data-field="상호"
+          data-type="date"
+        />
+        <DxColumn
+          data-field="대표자"
+        />
+        <DxColumn
+          data-field="영업자"
+        />
+        <DxColumn
+          data-field="신청서비스"
+        />
+        <DxColumn
+          data-field="부가서비스"
+        />
+      </DxDataGrid>
     </div>
-</template>
-
+  </template>
+  <script>
+  import {
+    DxDataGrid,
+    DxColumn,
+    DxPaging,
+    DxEditing,
+    DxPopup,
+    DxForm,
+    DxSelection,
+    DxSearchPanel
+  } from 'devextreme-vue/data-grid';
+  import { DxItem } from 'devextreme-vue/form';
+  import { employees, states } from './data.js';
+  
+  export default {
+    components: {
+      DxDataGrid,
+      DxColumn,
+      DxPaging,
+      DxEditing,
+      DxPopup,
+      DxForm,
+      DxItem,
+      DxSelection,
+      DxSearchPanel
+    },
+    data() {
+      return {
+        dataSource: employees,
+        states,
+      };
+    },
+  };
+  </script>
+  <style>
+  #data-grid-demo {
+    min-height: 700px;
+  }
+  .dx-select-checkbox {
+    display: inline-block !important;
+  }
+  </style>
