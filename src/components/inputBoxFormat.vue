13c2d604e0c8aa0de32621a7fda9a21234791ef4
antu_dev <tupa@townsoftvina.com>
2022-09-21 13:36:05 +0700

    update disable

--- src/components/inputBoxFormat.vue
+++ src/components/inputBoxFormat.vue
@@ -2,10 +2,11 @@
     <div @click="$event.target.select()">
         <DxNumberBox v-if="max!= '' && min!= ''" v-model:value="valueInput" value-change-event="keyup"
             style="width: 120px;height: 33px" @value-changed="changeValueInput" :format="format"
-            :show-spin-buttons="spinButtons" :show-clear-button="clearButton" :max="max" :min="min" />
+            :show-spin-buttons="spinButtons" :show-clear-button="clearButton" :max="max" :min="min" :disabled="disabled" />
 
         <DxNumberBox v-else v-model:value="valueInput" value-change-event="keyup" style="width: 120px;height: 33px"
-            @value-changed="changeValueInput" :show-spin-buttons="spinButtons" :show-clear-button="clearButton" />
+            @value-changed="changeValueInput" :show-spin-buttons="spinButtons" :show-clear-button="clearButton"
+            :disabled="disabled" />
     </div>
 </template>
 
@@ -19,7 +20,8 @@ export default defineComponent({
         min: Number,
         max: Number,
         spinButtons: Boolean,
-        clearButton: Boolean
+        clearButton: Boolean,
+        disabled:Boolean
 
     },
     components: {
