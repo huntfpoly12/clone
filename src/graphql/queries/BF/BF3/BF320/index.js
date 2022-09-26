41500a12e8798e12809dfa165ba0515c44d90e2d
antu_dev <tupa@townsoftvina.com>
2022-09-26 14:35:47 +0700

    update code

--- src/graphql/queries/BF/BF3/BF320/index.js
+++ src/graphql/queries/BF/BF3/BF320/index.js
@@ -1,15 +1,6 @@
-import gql from "graphql-tag";
+import getData from "./getData"; 
 const queries = {
-  findParters: gql`
-      query searchCompanies ($page: Int!, $rows: Int!, $excludeCancel: Boolean!) {
-        searchCompanies (filter: { page: $page, rows: $rows, excludeCancel: $excludeCancel }) {
-          datas {
-            name
-          }
-        }
-      }
-    `,
-
+    getData: getData
 };
 
 export default queries;
