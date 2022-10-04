50892779cf22801ffe52dd2d8ce091f71556c57d
HoangTrang2810 <tranght@townsoftvina.com>
2022-10-04 08:34:33 +0700

    screenrole

---
+++ src/graphql/queries/BF/BF2/BF210/searchScreenRoleGroups.js
@@ -0,0 +1,24 @@
+import gql from "graphql-tag";
+export default gql`
+  query searchScreenRoleGroups($page: Int!, $rows: Int!, $types: [String!]) {
+    searchScreenRoleGroups(filter: { page: $page, rows: $rows, type: $type }) {
+      totalCount
+      page
+      rows
+      datas {
+        id
+        name
+        type
+        screenRoles
+        lock
+        memo
+        createdAt
+        createdBy
+        updatedAt
+        updatedBy
+        ip
+        active
+      }
+    }
+  }
+`;
