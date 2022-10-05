32d3607c2d7e9337e47d2d1be30447bc4a57b4d9
snowman <dungtv@townsoftvina.com>
2022-10-05 10:41:38 +0700

    edit 320 popup

---
+++ src/graphql/mutations/BF/BF3/BF320/updateCompany.js
@@ -0,0 +1,72 @@
+import gql from "graphql-tag";
+export default gql`
+mutation updateCompany( $id: Int!, $detail: CompanyDetailInput, $president: CompanyPresidentInput, $cmsBank: CompanyCmsBankInput) {
+    updateCompany(id: $id, detail: $detail, president: $president,cmsBank: $cmsBank){
+        id
+    code
+    name
+    bizNumber
+    bizType
+    address
+    phone
+    presidentName
+    presidentMobilePhone
+    extendInfo{
+      detail{
+        name
+        zipcode
+        roadAddress
+        jibunAddress
+        addressExtend
+        addressDetail{
+          bcode
+          bname
+          buildingCode
+          buildingName
+          roadname
+          roadnameCode
+          sido
+          sigungu
+          sigunguCode
+          zonecode
+        }
+        phone
+        fax
+        licenseFileStorageId
+      }
+      president{
+        name
+        birthday
+        mobilePhone
+        email
+      }
+      cmsBank{
+        bankType
+        accountNumber
+        ownerBizNumber
+        ownerName
+        withdrawDay
+      }
+    }
+    sealFileStorageId
+    createdAt
+    createdBy
+    updatedAt
+    updatedBy
+    ip
+    active
+    seal{
+      name
+      url
+      createdAt
+      createdBy
+      updatedAt
+      updatedBy
+      ip
+      active
+    }
+    canceledAt
+    unpaidMonths
+  }
+}
+`
