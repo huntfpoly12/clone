590e5dd6d78c28e3a1d0bb363a583df64daa12f0
snowman <dungtv@townsoftvina.com>
2022-10-06 11:28:53 +0700

    edit history 340

--- src/graphql/queries/common/index.ts
+++ src/graphql/queries/common/index.ts
@@ -2,11 +2,16 @@ import getListSale from "./getListSale";
 import getListManager from "./getListManager";
 import getListPartner from "./getListPartner";
 import isUserRegistable from "./isUserRegistable";
+import getCompanyLogs from "./getCompanyLogs"; 
+import getSalesRepresentativeLogs from "./getSalesRepresentativeLogs"; 
+
 const queries = {
   getListSale: getListSale,
   getListManager: getListManager,
   getListPartner: getListPartner,
-  isUserRegistable: isUserRegistable
+  isUserRegistable: isUserRegistable,
+  getCompanyLogs:getCompanyLogs,
+  getSalesRepresentativeLogs:getSalesRepresentativeLogs
 };
 
 export default queries;
