a9d3a8ea433c2b81d5cbc59632edeb5b3e8586a0
snowman <dungtv@townsoftvina.com>
2022-10-10 17:33:17 +0700

    edit cm 130

---
+++ src/graphql/queries/CM/CM130/getWithholdingConfigPayItems.ts
@@ -0,0 +1,29 @@
+import gql from "graphql-tag";
+export default gql`
+    query
+    getWithholdingConfigPayItems( $companyId: Int!,$imputedYear: Int!,$useOnly: Boolean ) {
+        getWithholdingConfigPayItems(companyId : $companyId, imputedYear: $imputedYear, useOnly: $useOnly) {
+                itemCode
+                name
+                taxPayItemCode
+                taxPayItemName
+                taxfreePayItemCode
+                taxfreePayItemName
+                monthlyLimit
+                annualLimit
+                tax
+                taxFreeIncludeSubmission
+                use
+                sort
+                formula
+                createdAt
+                createdBy
+                updatedAt
+                updatedBy
+                ip
+                active
+                editable
+        }
+    }
+    
+`
