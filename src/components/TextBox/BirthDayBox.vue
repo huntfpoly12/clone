7d97537e465e5b7ca93647e6f4330f6c9b21450e
DESKTOP-5AK0JLF\HP <tupa@townsoftvina.com>
2022-11-01 09:25:53 +0700

    update code fix bug 310

--- src/components/TextBox/BirthDayBox.vue
+++ src/components/TextBox/BirthDayBox.vue
@@ -1,59 +1,19 @@
 <template>
-  <div>
-    <DxTextBox
-      :width="width"
-      value-change-event="keyup"
-      :show-clear-button="clearButton"
-      v-model:value="value"
-      :disabled="disabled"
-      :readOnly="readOnly"
-      @change="updateValue(value)"
-      :mask="mask"
-      :mask-invalid-message="maskMess"
-      :height="$config_styles.HeightInput"
-    >
-      <DxValidator>
-        <DxRequiredRule v-if="required" :message="messRequired" />
-        <DxPatternRule :pattern="pattern" :message="maskMess" />
-      </DxValidator>
-    </DxTextBox>
-  </div>
+    <div>
+        <DxTextBox :width="width" value-change-event="keyup" :show-clear-button="clearButton" v-model:value="value"
+            :disabled="disabled" :readOnly="readOnly" @change="updateValue(value)" :mask="mask"
+            :mask-invalid-message="maskMess" :height="$config_styles.HeightInput">
+            <DxValidator>
+                <DxRequiredRule v-if="required" :message="messRequired" />
+                <DxPatternRule :pattern="pattern" :message="maskMess" />
+            </DxValidator>
+        </DxTextBox>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref ,watch } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import {
-  DxValidator,
-  DxRequiredRule,
-  DxCompareRule,
-  DxEmailRule,
-  DxPatternRule,
-  DxStringLengthRule,
-  DxRangeRule,
-  DxAsyncRule,
-} from "devextreme-vue/validator";
-import DxTextBox from "devextreme-vue/text-box";
-export default defineComponent({
-  props: {
-    required: {
-      type: Boolean,
-      default: false,
-    },
-    messRequired: {
-      type: String,
-      default: "Input is required :) !!!!",
-    },
-    width: String,
-    clearButton: Boolean,
-    disabled: Boolean,
-    valueInput: {
-      type: String,
-      default: "",
-    },
-    readOnly: Boolean,
-  },
-  components: {
-    DxTextBox,
     DxValidator,
     DxRequiredRule,
     DxCompareRule,
@@ -62,29 +22,60 @@ export default defineComponent({
     DxStringLengthRule,
     DxRangeRule,
     DxAsyncRule,
-  },
-  setup(props, { emit }) {
-    const mask = ref("0000-00-00");
-    const maskMess = ref("The value must have a correct date format");
-    const value = ref(props.valueInput.replaceAll('-', ''));
-    const pattern = ref(/((19|20)\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01]))/);
-    const updateValue = (value: any) => {
-      emit("update:valueInput", value);
-    };
+} from "devextreme-vue/validator";
+import DxTextBox from "devextreme-vue/text-box";
+export default defineComponent({
+    props: {
+        required: {
+            type: Boolean,
+            default: false,
+        },
+        messRequired: {
+            type: String,
+            default: "이항목은 필수 입력사항입니다!",
+        },
+        width: String,
+        clearButton: Boolean,
+        disabled: Boolean,
+        valueInput: {
+            type: String,
+            default: "",
+        },
+        readOnly: Boolean,
+    },
+    components: {
+        DxTextBox,
+        DxValidator,
+        DxRequiredRule,
+        DxCompareRule,
+        DxEmailRule,
+        DxPatternRule,
+        DxStringLengthRule,
+        DxRangeRule,
+        DxAsyncRule,
+    },
+    setup(props, { emit }) {
+        const mask = ref("0000-00-00");
+        const maskMess = ref("The value must have a correct date format");
+        const value = ref(props.valueInput.replaceAll('-', ''));
+        const pattern = ref(/((19|20)\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01]))/);
+        const updateValue = (value: any) => {
+            emit("update:valueInput", value);
+        };
 
-    watch(
-      () => props.valueInput,
-      (newValue) => {
-        value.value = newValue.replaceAll('-', '');
-      }
-    );
-    return {
-      updateValue,
-      value,
-      mask,
-      maskMess,
-      pattern,
-    };
-  },
+        watch(
+            () => props.valueInput,
+            (newValue) => {
+                value.value = newValue.replaceAll('-', '');
+            }
+        );
+        return {
+            updateValue,
+            value,
+            mask,
+            maskMess,
+            pattern,
+        };
+    },
 });
 </script>
