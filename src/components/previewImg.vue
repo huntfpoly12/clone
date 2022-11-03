a8af04d97d6c2dbcc764b82f9de426e36d87a326
DESKTOP-5AK0JLF\HP <tupa@townsoftvina.com>
2022-11-03 13:53:10 +0700

    update code

---
+++ src/components/previewImg.vue
@@ -0,0 +1,21 @@
+<template>
+    <a-col>
+        <div v-if="dataImage" class="img-preview">
+            <a-image :src="dataImage.url" style="height: 200px;" />
+        </div>
+        <div v-else class="img-preview">
+            <img src="../../src/assets/images/imgdefault.jpg" style="height: 200px;" />
+        </div> 
+    </a-col>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    components: {
+
+    },
+    props: ['width', 'height', 'activePreview','dataImage']
+
+})
+</script>
