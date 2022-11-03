470faa3ed57dbaad3a3911f10c7c4cd8f2558f9c
Nghĩa Huy <nghianh@townsoftvina.com>
2022-11-03 09:54:24 +0700

    fix bug 220

---
+++ src/views/BF/BF2/BF220/utils/index.ts
@@ -0,0 +1,10 @@
+export const dataSearchUtils = {
+    page: 1,
+    rows: 20,
+    types: ["m", "r", "p"]
+}
+export const buttonSearchUtils = {
+    typeSevice1: true,
+    typeSevice2: true,
+    typeSevice3: true
+}
