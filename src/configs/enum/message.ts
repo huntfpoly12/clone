456b0f18453e7e1fde4bc36c5fdc7c5ee3ed51c9
HAN-A-RAM <111416490+HAN-A-RAM@users.noreply.github.com>
2022-11-07 10:19:04 +0700

    Update message.ts

--- src/configs/enum/message.ts
+++ src/configs/enum/message.ts
@@ -10,11 +10,13 @@ export class Message extends BaseType {
     * $messages.getCommonMessage('xxxxxx','001') --> return  계좌번호,계좌비밀 xxxxxx 번호,사업자등록번호
     */
     private static COMMON = new Message({
+      // SAVE Button Click 
         '101': {"저장되었습니다.", "iconInfo"}, // Save successful!
         '102': {"저장실패! {object}는 필수입력 사항입니다.", "iconError"}, // {object} is required field.
         '103': {"저장실패! {object}는 최소 {minCount}자 이상입니다.", "iconError"}, // {object} more then {minCount} characters.
         '104': {"저장실패! {object}가 정해진 글자수를 초과하였습니다.", "iconError"}, // {object} exceeds the maximum number of characters.
         '105': {"저장실패! {object}가 올바른 형식이 아닙니다.", "iconError"}, // {object} has incorrect format.
+      // SEARCh Button Click
         '201': {"조회되었습니다.", "iconInfo"}, // Search successful!
     });
 
@@ -25,23 +27,14 @@ export class Message extends BaseType {
      * for dev will use like this
      * $messages.getMessage('BF340','001') --> return  계좌번호,계좌비밀번호,사업자등록번호
      */
-    public static BF310 = new Message({
-        '001': "계좌번호,계좌비밀번호,사업자등록번호",
-        '002': "계좌번호,계좌비밀번호,생년월일(YYMMDD0000000)",
-    });
-
-    public static BF320 = new Message({
-        '001': "계좌번호,계좌비밀번호,사업자등록번호",
-        '002': "계좌번호,계좌비밀번호,생년월일(YYMMDD0000000)",
+    public static BF210 = new Message({
+        '001': "회원ID가 이미 사용중입니다",
     });
 
     public static BF340 = new Message({
         '001': "영업자명이 이미 사용중입니다.",
         '002': "해지(또는 숨김)하면 본 영업자와 연계된 모든 사업자는 본사로 이관됩니다. 해지(또는 숨김)하려면 <확인>을 입력한 후 완료를 누르세요.",
     });
-
-
-
 
     private static _ = Message.closeEnum();
 
