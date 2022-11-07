ae210b9329ae3aec17c2677cb519b6f29844f681
HAN-A-RAM <111416490+HAN-A-RAM@users.noreply.github.com>
2022-11-07 09:30:40 +0700

    Update message.ts

--- src/configs/enum/message.ts
+++ src/configs/enum/message.ts
@@ -10,11 +10,12 @@ export class Message extends BaseType {
     * $messages.getCommonMessage('xxxxxx','001') --> return  계좌번호,계좌비밀 xxxxxx 번호,사업자등록번호
     */
     private static COMMON = new Message({
-        '101': {"저장되었습니다.", "iconInfo" }, // Save successful!
+        '101': {"저장되었습니다.", "iconInfo"}, // Save successful!
         '102': {"저장실패! {object}는 필수입력 사항입니다.", "iconError"}, // {object} is required field.
         '103': {"저장실패! {object}는 최소 {minCount}자 이상입니다.", "iconError"}, // {object} more then {minCount} characters.
         '104': {"저장실패! {object}가 정해진 글자수를 초과하였습니다.", "iconError"}, // {object} exceeds the maximum number of characters.
         '105': {"저장실패! {object}가 올바른 형식이 아닙니다.", "iconError"}, // {object} has incorrect format.
+        '201': {"조회되었습니다.", "iconInfo"}, // Search successful!
     });
 
     /** 여기에 각 화면에 대한 메시지 추가 
