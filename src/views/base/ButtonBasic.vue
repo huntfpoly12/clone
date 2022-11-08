98fd9d8a50619590495d8a8a2d0ce19393c5a503
DESKTOP-5AK0JLF\HP <tupa@townsoftvina.com>
2022-11-08 16:13:30 +0700

    update code checkbox group

--- src/views/base/ButtonBasic.vue
+++ src/views/base/ButtonBasic.vue
@@ -137,7 +137,7 @@ export default defineComponent({
         { id: 3, text: 'High' }])
 
         const valueCheckBox = ref(false)
-        const valueRadioBox = ref(arrayRadioCheck.value[3])
+        const valueRadioBox = ref(3)
         const valueSwitchChange = ref(true)
 
         return {
