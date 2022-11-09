8666f87de1f3e52d2cf3ef1a45a6026ee10f9d81
dungtv <dungtv@townsoftvina.com>
2022-11-09 17:03:22 +0700

    edit  birdday box

--- src/components/common/BirthDayBox.vue
+++ src/components/common/BirthDayBox.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    {{ value }} {{ dateInput }}
     <DxTextBox
       :width="width"
       value-change-event="input"
@@ -14,23 +15,23 @@
     >
       <DxValidator>
         <DxRequiredRule v-if="required" :message="messageRequired" />
-        <DxPatternRule :pattern="pattern" :message="maskMess" />
+        <DxCustomRule :reevaluate="true"
+                :message="maskMess"
+                :validation-callback="validateDate"
+            />
       </DxValidator>
     </DxTextBox>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref ,watch , getCurrentInstance } from "vue";
+import { defineComponent, ref, watch, getCurrentInstance } from "vue";
+import dayjs, { Dayjs } from 'dayjs';
 import {
   DxValidator,
   DxRequiredRule,
-  DxCompareRule,
-  DxEmailRule,
   DxPatternRule,
-  DxStringLengthRule,
-  DxRangeRule,
-  DxAsyncRule,
+  DxCustomRule
 } from "devextreme-vue/validator";
 import DxTextBox from "devextreme-vue/text-box";
 export default defineComponent({
@@ -56,24 +57,20 @@ export default defineComponent({
     DxTextBox,
     DxValidator,
     DxRequiredRule,
-    DxCompareRule,
-    DxEmailRule,
     DxPatternRule,
-    DxStringLengthRule,
-    DxRangeRule,
-    DxAsyncRule,
+    DxCustomRule
   },
   setup(props, { emit }) {
-    const app : any= getCurrentInstance()
+    const app: any = getCurrentInstance();
     const messages = app.appContext.config.globalProperties.$messages;
     const mask = ref("0000-00-00");
-    const maskMess = ref(messages.getCommonMessage('105').message);
-    const messageRequired = ref(messages.getCommonMessage('102').message);
-    if(props.messRequired != ""){
+    const dateInput = ref("");
+    const maskMess = ref(messages.getCommonMessage("105").message);
+    const messageRequired = ref(messages.getCommonMessage("102").message);
+    if (props.messRequired != "") {
       messageRequired.value = props.messRequired;
     }
-    const value = ref(props.valueInput.replaceAll('-', ''));
-    const pattern = ref(/((19|20)\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01]))/);
+    const value = ref(props.valueInput.replaceAll("-", ""));
     const updateValue = (value: any) => {
       emit("update:valueInput", value);
     };
@@ -81,16 +78,24 @@ export default defineComponent({
     watch(
       () => props.valueInput,
       (newValue) => {
-        value.value = newValue.replaceAll('-', '');
+        value.value = newValue.replaceAll("-", "");
       }
     );
+
+    const validateDate = (e:any)=>{
+      let date = e.value;
+      let dtFormat = date.slice(0, 4) + "-" + date.slice(4, 6) + "-" + date.slice(6, 8);
+      return dayjs(dtFormat, 'YYYY-MM-DD', true).isValid();
+    }
+
     return {
       updateValue,
       value,
       mask,
       maskMess,
       messageRequired,
-      pattern,
+      dateInput,
+      validateDate
     };
   },
 });
