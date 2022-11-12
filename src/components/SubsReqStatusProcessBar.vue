e8b428653322bad9ce61f865cfc94044f07feb79
dungtv <dungtv@townsoftvina.com>
2022-11-12 10:07:13 +0700

    edit

--- src/components/SubsReqStatusProcessBar.vue
+++ src/components/SubsReqStatusProcessBar.vue
@@ -2,8 +2,8 @@
   <div style="display: flex">
     <div class="application" :class="[ valueInput ==  SubscriptionRequestStatus.신청 ? 'active-app' : '']" @click="setStatus(SubscriptionRequestStatus.신청)"></div>
     <div class="under-examination" :class="[ valueInput ==  SubscriptionRequestStatus.심사중 ? 'active-under-eX' : '']" @click="setStatus(SubscriptionRequestStatus.심사중)"></div>
-    <div class="approval" :class="[ valueInput ==  SubscriptionRequestStatus.승인 ? 'active-approval' : '']" @click="setStatus(SubscriptionRequestStatus.승인)"></div>
-    <div class="application-rejected" :class="[ valueInput ==  SubscriptionRequestStatus.반려 ? 'active-app-rejected' : '']" @click="setStatus(SubscriptionRequestStatus.반려)"></div>
+    <div class="approval" :class="[ valueInput ==  SubscriptionRequestStatus.반려 ? 'active-approval' : '']" @click="setStatus(SubscriptionRequestStatus.반려)"></div>
+    <div class="application-rejected" :class="[ valueInput ==  SubscriptionRequestStatus.승인 ? 'active-app-rejected' : '']" @click="setStatus(SubscriptionRequestStatus.승인)"></div>
     <div style="width: 50px;font-weight: bold;color: grey;">
         {{getEnumKey(SubscriptionRequestStatus,valueInput)}}
     </div>
@@ -49,26 +49,26 @@ export default defineComponent({
     width: 70px;
     height: 20px;
     margin-right: 5px;
-    border: 1px solid red;
+    border: 1px solid #FF4D4F;
     cursor: pointer;
     background-color: white;
 }
 
 .active-app{
-    background-color: red;
+    background-color: #FF4D4F;
 }
 
 .under-examination{
     width: 70px;
     height: 20px;
     margin-right: 5px;
-    border: 1px solid blue;
+    border: 1px solid #1990FF;
     cursor: pointer;
     background-color: white;
 }
 
 .active-under-eX{
-    background-color: blue;
+    background-color: #1990FF;
 }
 
 .approval{
@@ -86,13 +86,13 @@ export default defineComponent({
     width: 70px;
     height: 20px;
     margin-right: 5px;
-    border: 1px solid green;
+    border: 1px solid #52C41A;
     cursor: pointer;
     background-color: white;
 }
 
 .active-app-rejected{
-    background-color: green;  
+    background-color: #52C41A;  
 }
 
 </style>
