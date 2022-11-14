ebdf39e1494a46d686ea98861a12230d636c52c9
dungtv <dungtv@townsoftvina.com>
2022-11-14 11:20:59 +0700

    edit confirm close popup

--- src/utils/comfirmClosePopup.ts
+++ src/utils/comfirmClosePopup.ts
@@ -2,23 +2,22 @@
 import { ExclamationCircleOutlined } from '@ant-design/icons-vue';
 import { createVNode } from 'vue';
 import { Modal } from 'ant-design-vue';
+import { Message } from "../configs/enum";
 
-export default () => {
-    let status = -1
-    Modal.confirm({
-        title: 'Do you want to delete these items?',
+export default (callback: Function) => {
+    let status = false
+    const message = Message.getCommonMessage('301').message;
+   Modal.confirm({
+        title: message,
         icon: createVNode(ExclamationCircleOutlined),
-        content: 'When clicked the OK button, this dialog will be closed after 1 second',
         onOk() {
-            return new Promise((resolve, reject) => {
-                resolve(true)
-            }).catch(() => console.log('Oops errors!'));
+            callback();
         },
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        onCancel() { status = 0 },
+        onCancel() { status = false },
+        style:{
+            background:'red'
+        }
     });
 
-    if (status != -1) {
-        return status
-    }
+   return status;
 }
