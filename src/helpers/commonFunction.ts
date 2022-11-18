80c5ab155d9bd1cbb07a85e6c484291203c2aa76
AnTus_Dev_Vippro <tupa@townsoftvina.com>
2022-11-18 14:54:34 +0700

    Clear code page BF-3xx

--- src/helpers/commonFunction.ts
+++ src/helpers/commonFunction.ts
@@ -14,4 +14,28 @@ if (token) {
     }
 }
 
-export { companyId, userType, userId, infoUser }
+
+// common export data 
+import { Workbook } from "exceljs";
+import { exportDataGrid } from "devextreme/excel_exporter";
+import { saveAs } from "file-saver-es";
+
+const onExportingCommon = (component: any, cancel: boolean, name: String) => {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet("employees");
+    exportDataGrid({
+        component: component,
+        worksheet,
+        autoFilterEnabled: true,
+    }).then(() => {
+        workbook.xlsx.writeBuffer().then((buffer) => {
+            saveAs(
+                new Blob([buffer], { type: "application/octet-stream" }),
+                name + ".xlsx"
+            );
+        });
+    });
+    cancel = true;
+}
+
+export { companyId, userType, userId, infoUser, onExportingCommon }
