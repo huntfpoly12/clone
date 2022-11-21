1956f32d27d9c24c248e3f7ba8eb0dc283ab4dd8
dungtv <dungtv@townsoftvina.com>
2022-11-21 17:03:57 +0700

    edit 320

---
+++ src/views/PA/PA2/PA230/utils/index.ts
@@ -0,0 +1,11 @@
+export const radioCheckDataSearch = [
+    { id: 1, text: '전체' },
+    { id: 2, text: '계속' },
+    { id: 3, text: '중도' }];
+
+export const radioCheckData = [
+    { id: 1, text: '소득자별근로소득원천징수부(전체)' },
+    { id: 2, text: 'I. 근로소득지급명세서' },
+    { id: 3, text: 'II. 비과세소득' },
+    { id: 4, text: 'III. 근로소득원천징수액등' }
+];
