d4c130f124927963d17338549573efc0e16bec09
Tran Mai Son <sontm@townsoftvina.com>
2022-11-22 15:28:28 +0700

    router pa-430

--- src/views/PA/PA4/PA430/index.vue
+++ src/views/PA/PA4/PA430/index.vue
@@ -1,47 +1,231 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <a-spin :spinning="loading" size="large">
-    <action-header title="퇴직소득원천징수영수증" />
+    <action-header title="계약정보관리&심사" @actionSearch="searching" />
     <div id="pa-430">
-      <div class="page-content">
-        <a-row>
-          <a-col :span="36" class="custom-layout">
-            <div>
-              <div>
-                <div>
-                  <span>귀속/지급: </span>
-                  <switch-basic
-                    :textCheck="'지급'"
-                    :textUnCheck="'귀속'"
-                  ></switch-basic>
-                </div>
-                <div>
-                  <span> 기간 :</span>
-                </div>
-                <div>
-                  <span>구분 :</span>
-                </div>
-                <div>
-                  <radio-group
-                    :arrayValue="arrayRadioCheck"
-                    v-model:valueRadioCheck="valueRadioBox"
-                    :layoutCustom="'horizontal'"
-                  />
-                </div>
+      <div class="search-form">
+        <a-row :gutter="[24, 8]">
+          <div class="dflex custom-flex">
+            <a-col>
+              <label class="lable-item">귀속/지급 :</label>
+              <switch-basic
+                v-model:valueSwitch="originData.excludeCancel"
+                :textCheck="'포함'"
+                :textUnCheck="'제외'"
+              />
+            </a-col>
+            <a-col>
+              <div class="dflex custom-flex">
+                <label class="lable-item">영수일:</label>
+                <a-range-picker
+                  :placeholder="['Start month', 'End month']"
+                  format="YYYY-MM"
+                  :value="value"
+                  :mode="mode2"
+                  @panelChange="handlePanelChange2"
+                  @change="handleChange"
+                />
               </div>
+            </a-col>
+
+            <a-col>
+              <div class="dflex custom-flex">
+                <label class="lable-item">귀속/지급 :</label>
+                <radio-group
+                  :arrayValue="arrayRadioCheck"
+                  v-model:valueRadioCheck="valueRadioBox"
+                  :layoutCustom="'horizontal'"
+                />
+              </div>
+            </a-col>
+          </div>
+
+          <a-col>
+            <div class="dflex custom-flex">
+              <label class="lable-item">서식 설정 :</label>
+              <span>
+                본 설정으로 적용된 서식으로 출력 및 메일발송 됩니다.
+              </span>
             </div>
           </a-col>
+          <a-col>
+            <div class="dflex custom-flex">
+              <label class="lable-item">영수일:</label>
+              <date-time-box
+                width="300px"
+                dateFormat="YYYY-MM-DD"
+              ></date-time-box>
+            </div>
+          </a-col>
+          <a-col>
+            <label class="lable-item">소득자보관용 :</label>
+            <switch-basic
+              v-model:valueSwitch="originData.excludeCancel2"
+              :textCheck="'발행자보관용'"
+              :textUnCheck="'발행자보관용'"
+            />
+          </a-col>
         </a-row>
+      </div>
+      <div class="page-content">
+        <DxDataGrid
+          :show-row-lines="true"
+          :hoverStateEnabled="true"
+          :data-source="dataSource"
+          :show-borders="true"
+          key-expr="id"
+          @exporting="onExporting"
+          :allow-column-reordering="move_column"
+          :allow-column-resizing="colomn_resize"
+          :column-auto-width="true"
+        >
+          <DxSearchPanel :visible="true" :highlight-case-sensitive="true" />
+          <DxPaging :page-size="rowTable" />
+          <DxExport :enabled="true" :allow-export-selected-data="true" />
+          <DxToolbar>
+            <DxItem name="exportButton" />
+            <DxItem name="page" template="pagination-table" location="after" />
+            <DxItem name="searchPanel" />
+          </DxToolbar>
+          <template #pagination-table>
+            <div v-if="rowTable > originData.rows">
+              <a-pagination
+                v-model:current="originData.page"
+                v-model:page-size="originData.rows"
+                :total="rowTable"
+                show-less-items
+                @change="searching"
+              />
+            </div>
+          </template>
+          <DxColumn data-field="checkbox" caption="Select Box" />
+          <DxColumn data-field="employInfo" caption="사원" />
+          <DxColumn data-field="phancong" caption="구분 " />
+          <DxColumn
+            data-field="createdAt"
+            caption="입사일 (정산시작일)"
+            cell-template="createdat-cell"
+            data-type="date"
+          />
+          <DxColumn
+            data-field="createdAt"
+            caption="퇴사일 (정산종료일)"
+            cell-template="createdat-cell"
+            data-type="date"
+          />
+          <DxColumn
+            data-field="createdAt"
+            caption="귀속연월"
+            cell-template="createdat-cell"
+            data-type="date"
+          />
+          <DxColumn
+            data-field="createdAt"
+            caption="지급연월"
+            cell-template="createdat-cell"
+            data-type="date"
+          />
+          <DxColumn data-field="companyName" caption="상호" />
+          <DxColumn data-field="companyAddress" caption="주소" />
+          <DxColumn data-field="presidentName" caption="대표자" />
+          <DxColumn caption="신청서비스" cell-template="acc-service" />
+          <template #acc-service="{ data }">
+            <span
+              >회계
+              <a-popover>
+                <template #content>
+                  <a-table
+                    :columns="[
+                      {
+                        title: 'Name',
+                        dataIndex: 'name',
+                      },
+                      {
+                        title: 'Year Month',
+                        dataIndex: 'startYearMonth',
+                      },
+                    ]"
+                    :data-source="data.data.simpleAccountingInfos"
+                    bordered
+                    :pagination="false"
+                  >
+                  </a-table>
+                </template>
+                <a-tag v-if="data.data.simpleAccountingInfos"
+                  >{{ data.data.simpleAccountingInfos.length }}
+                </a-tag>
+              </a-popover>
+            </span>
+            <span
+              >원천
+              <a-popover>
+                <template #content>
+                  <a-table
+                    :columns="[
+                      {
+                        title: 'Name',
+                        dataIndex: 'name',
+                      },
+                      {
+                        title: 'Year Month',
+                        dataIndex: 'startYearMonth',
+                      },
+                    ]"
+                    :data-source="[data.data.simpleWithholdingInfo]"
+                    bordered
+                    :pagination="false"
+                  >
+                  </a-table>
+                </template>
+                <a-tag>1</a-tag>
+              </a-popover>
+            </span>
+          </template>
+          <DxColumn :width="80" cell-template="pupop" type="buttons" />
+          <template #pupop="{ data }" class="custom-action">
+            <div class="custom-action">
+              <div
+                style="color: blue; cursor: pointer"
+                @click="setModalVisible(data)"
+              >
+                편집
+              </div>
+            </div>
+          </template>
+        </DxDataGrid>
+        <div class="pagination-table" v-if="rowTable > originData.rows">
+          <a-pagination
+            v-model:current="originData.page"
+            v-model:page-size="originData.rows"
+            :total="rowTable"
+            show-less-items
+            @change="searching"
+          />
+        </div>
+        <BF310Popup
+          :modalStatus="modalStatus"
+          @closePopup="modalStatus = false"
+          :data="idSubRequest"
+        />
       </div>
     </div>
   </a-spin>
 </template>
-
 <script lang="ts">
-import { defineComponent, ref, watch, reactive, computed } from "vue";
+import { ref, defineComponent, reactive, watch, computed } from "vue";
 import { useStore } from "vuex";
 import { useQuery } from "@vue/apollo-composable";
-import notification from "../../../../utils/notification";
-import queries from "../../../../graphql/queries/PA/PA6/PA610/index";
+import dayjs from "dayjs";
+import {
+  SearchOutlined,
+  EditOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  MailOutlined,
+  PrinterOutlined,
+  DeleteOutlined,
+  SaveOutlined,
+} from "@ant-design/icons-vue";
 import {
   DxDataGrid,
   DxColumn,
@@ -49,30 +233,15 @@ import {
   DxExport,
   DxSelection,
   DxSearchPanel,
+  DxPager,
   DxToolbar,
-  DxEditing,
-  DxGrouping,
-  DxScrolling,
   DxItem,
 } from "devextreme-vue/data-grid";
-import {
-  EditOutlined,
-  HistoryOutlined,
-  SearchOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  MailOutlined,
-  PrinterOutlined,
-  DeleteOutlined,
-  SaveOutlined,
-  InfoCircleFilled,
-} from "@ant-design/icons-vue";
+import BF310Popup from "./components/BF310Popup.vue";
+import queries from "../../../../graphql/queries/BF/BF3/BF310/index";
+import { dataSearchIndex } from "./utils/index";
 import { onExportingCommon } from "../../../../helpers/commonFunction";
-import { origindata, ArrForeigner } from "./utils";
-import DxButton from "devextreme-vue/button";
-import { companyId } from "../../../../../src/helpers/commonFunction";
-import dayjs from "dayjs";
-import { Moment } from "moment";
+
 export default defineComponent({
   components: {
     DxDataGrid,
@@ -81,45 +250,125 @@ export default defineComponent({
     DxSelection,
     DxExport,
     DxSearchPanel,
-    DxScrolling,
-    EditOutlined,
-    HistoryOutlined,
-    DxToolbar,
-    DxEditing,
-    DxGrouping,
-    DxItem,
+    BF310Popup,
     SearchOutlined,
+    EditOutlined,
+    DxPager,
     MenuFoldOutlined,
     MenuUnfoldOutlined,
     MailOutlined,
     PrinterOutlined,
     DeleteOutlined,
     SaveOutlined,
-    ArrForeigner,
-    DxButton,
-    InfoCircleFilled,
+    DxToolbar,
+    DxItem,
   },
   setup() {
-    const loading = ref(false);
+    const store = useStore();
+    const per_page = computed(() => store.state.settings.per_page);
+    const move_column = computed(() => store.state.settings.move_column);
+    const colomn_resize = computed(() => store.state.settings.colomn_resize);
+    const rangeDate = ref([dayjs().subtract(1, "year"), dayjs()]);
+    const dataSource = ref([]);
+    const modalStatus = ref(false);
+    const idSubRequest = ref();
+    const statuses: any = ref([]);
+    const trigger = ref<boolean>(true);
+    const rowTable = ref();
+    const originData = reactive({
+      ...dataSearchIndex,
+      rows: per_page,
+    });
     const arrayRadioCheck = ref([
-      { id: 0, text: "전체"},
+      { id: 0, text: "전체" },
       { id: 1, text: "퇴직소득" },
-      { id: 2, text: "중간정산"},
+      { id: 2, text: "중간정산" },
     ]);
     const valueRadioBox = ref(arrayRadioCheck.value[2]);
-// const selectBoxBackground=ref(arrayRadioCheck.value)
-// console.log('selectBoxbackground', selectBoxBackground.value)
-    // ================GRAPQL==============================================
-
-    // ================FUNCTION============================================
-
+    const mode2 = ref(["month", "month"]);
+    const value = ref([]);
+    const handleChange = (val: any) => {
+      value.value = val;
+    };
+    const handlePanelChange2 = (val: any, mode: any) => {
+      value.value = val;
+      mode2.value = [
+        mode[0] === "date" ? "month" : mode[0],
+        mode[1] === "date" ? "month" : mode[1],
+      ];
+    };
+    const setModalVisible = (data: any) => {
+      idSubRequest.value = data.data.id;
+      modalStatus.value = true;
+    };
+    const {
+      refetch: refetchData,
+      loading,
+      error,
+      result,
+    } = useQuery(
+      queries.searchSubscriptionRequests,
+      { filter: originData },
+      () => ({
+        enabled: trigger.value,
+        fetchPolicy: "no-cache",
+      })
+    );
+    const onExporting = (e: { component: any; cancel: boolean }) => {
+      onExportingCommon(e.component, e.cancel, "계약정보관리&심사");
+    };
+    const getColorTag = (data: any) => {
+      if (data == 10) {
+        return { name: "red", tag_name: "신청" };
+      } else if (data == 20) {
+        return { name: "blue", tag_name: "심사중" };
+      } else if (data == 30) {
+        return { name: "green", tag_name: "승인" };
+      } else if (data == 99) {
+        return { name: "grey", tag_name: "반려" };
+      }
+    };
+    const formarDate = (date: any) => {
+      return dayjs(date).format("YYYY-MM-DD");
+    };
+    const searching = () => {
+      originData.startDate = formarDate(rangeDate.value[0]);
+      originData.finishDate = formarDate(rangeDate.value[1]);
+      originData.statuses =
+        statuses.value == 0 ? [10, 20, 30, 99] : statuses.value;
+      trigger.value = true;
+      refetchData();
+    };
+    watch(result, (value) => {
+      if (value) {
+        rowTable.value = value.searchSubscriptionRequests.totalCount;
+        dataSource.value = value.searchSubscriptionRequests.datas;
+        trigger.value = false;
+      }
+    });
     return {
       loading,
       valueRadioBox,
       arrayRadioCheck,
+      mode2,
+      value,
+      handleChange,
+      handlePanelChange2,
+      move_column,
+      colomn_resize,
+      rangeDate,
+      idSubRequest,
+      dataSource,
+      modalStatus,
+      rowTable,
+      setModalVisible,
+      originData,
+      statuses,
+      searching,
+      getColorTag,
+      onExporting,
     };
   },
 });
 </script>
-<style scoped lang="scss" src="./style/style.scss">
-</style>
+<style lang="scss" scoped src="./style/style.scss" />
--- src/views/PA/PA4/PA430/utils/index.ts
+++ src/views/PA/PA4/PA430/utils/index.ts
@@ -1,18 +1,139 @@
-interface Origindata {
-    page: number,
-    rows: number,
-    statuses: number[],
-    grade: null | number,
-    name: string,
-    code: string,
-}
-export const origindata: Origindata = {
-    page: 1,
-    rows: 10,
-    statuses: [1],
-    grade: null,
-    name: "",
+// Data Popup
+export const bizTypeItems = [{ id: 1, text: '법인사업자' }, { id: 2, text: '개인사업자' }];
+export const initialFormState = {
+    id: null,
+    status: 10,
     code: "",
+    companyName: "",
+    companyBizNumber: "",
+    companyAddress: "",
+    presidentName: "",
+    simpleAccountingInfos: {
+        name: "",
+        startYearMonth: "",
+    },
+    processedAt: "",
+    approvedAt: "",
+    rejectedAt: "",
+    processedBy: "",
+    rejectedBy: "",
+    approvedBy: "",
+    content: {
+        agreements: {
+            terms: true,
+            personalInfo: true,
+            accountingService: true,
+            withholdingService: true,
+        },
+        company: {
+            name: "",
+            zipcode: "",
+            roadAddress: "",
+            jibunAddress: "",
+            addressExtend: "",
+            addressDetail: {
+                bcode: "",
+                bname: "",
+                buildingCode: "",
+                buildingName: "",
+                roadname: "",
+                roadnameCode: "",
+                sido: "",
+                sigungu: "",
+                sigunguCode: "",
+                zonecode: "",
+            },
+            phone: "",
+            fax: "",
+            licenseFileStorageId: "",
+            bizType: 0,
+            residentId: "",
+            // license: "",
+        },
+        president: {
+            name: "",
+            birthday: "",
+            mobilePhone: "",
+            email: "",
+        },
+        accounting: {
+            facilityBusinesses: [],
+            accountingServiceTypes: [],
+        },
+        withholding: {
+            startYearMonth: "",
+            capacity: 1234,
+            withholdingServiceTypes: [],
+        },
+        cmsBank: {
+            bankType: "",
+            accountNumber: "",
+            ownerName: "",
+            ownerBizNumber: "",
+            withdrawDay: "",
+        },
+        extra: {
+            salesRepresentativeId: 1,
+            comment: "",
+        }
+    },
+    compactSalesRepresentative: {
+        id: "",
+        code: "",
+        name: "",
+        active: "",
+    },
+    institutionNumber: "",
+
+    memo: "",
+    createdAt: "",
+    createdBy: "",
+    updatedBy: "",
+    ip: "",
+    active: "",
+};
+
+/** Application (신청) - 10 */
+/** Under examination (심사중) - 20 */
+/** Approval (승인) - 30 */
+/** Application rejected (반려) - 99 */
+export const initialDataStatus = [
+    { name: "승인", date: "2022-08-25", color: "green", value: 30 },
+    { name: "신청", date: "2022-08-25", color: "red", value: 10 },
+    { name: "심사중", date: "2022-08-25", color: "blue", value: 20 },
+    { name: "반려", date: "2022-08-25", color: "grey", value: 99 },
+];
+
+export const inputInCollapse = [
+    { key: 2, input_name: ['companyName', 'companyBizNumber', 'residentId', 'company-phone'] },
+    { key: 3, input_name: ['president-name', 'president-mobilePhone', 'president-email'] },
+    { key: 4, input_name: ['longTermCareInstitutionNumber'] },
+    { key: 5, input_name: ['withholding-capacity'] },
+    { key: 6, input_name: ['cmsBank-accountNumber', 'cmsBank-ownerName', 'cmsBank-ownerBizNumber'] },
+]
+
+
+// Data index
+
+
+// Data searching default
+export const dataSearchIndex = {
+    page: 1,
+    salesRepresentativeId: 0,
+    startDate: '',
+    finishDate: '',
+    accounting: true,
+    withholding: true,
+    companyName: "",
+    presidentName: "",
+    statuses: [10, 20, 30, 99],
+    excludeCancel: true,
+    excludeCancel2:true,
+
 }
 
-export const ArrForeigner = [{ id: 1, text: '내국인' }, { id: 2, text: '외국인' }]
+export const initialArrayRadioWithdrawDay = [
+    { id: '매월 5일', text: '매월 5일' },
+    { id: '매월 12일', text: '매월 12일' },
+    { id: '매월 19일', text: '매월 19일' },
+]
