13a277fe496f52fb683aa5902255f0997f86e004
AnTus <tupa@townsoftvina.com>
2022-11-22 09:50:54 +0700

    update code page 610

--- src/views/PA/PA6/PA610/utils/index.ts
+++ src/views/PA/PA6/PA610/utils/index.ts
@@ -15,4 +15,4 @@ export const origindata: Origindata = {
     code: "",
 }
 
-export const ArrForeigner = [{ id: 1, text: '내국인' }, { id: 2, text: '외국인' }]
+export const ArrForeigner = [{ id: false, text: '내국인' }, { id: true, text: '외국인' }]
