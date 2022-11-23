78ecd421bcc8f2efbad3636604717d975d617b97
doankhiem <khiembi@townsoftvina.com>
2022-11-23 10:34:19 +0700

    config pa630

--- src/views/PA/PA6/PA630/index.vue
+++ src/views/PA/PA6/PA630/index.vue
@@ -1,111 +1,106 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <action-header title="사업소득자등록" />
-    <div id="pa-610">
-        <div class="page-content">
-            <a-row>
-                <a-col :span="3" class="total-user">
-                    <div>
-                        <span>{{ dataSource.length }}</span>
-                        <br>
-                        <span>전체</span>
-                    </div>
-                    <div>
-                        <i class="dx-icon-user"></i>
-                    </div>
-                </a-col>
-                <a-col :span="21"></a-col>
-                <a-col :span="24">
-                    <a-spin :spinning="loadingGetEmployeeBusinesses" size="large">
-                        <div>
-                            <DxDataGrid :show-row-lines="true" :hoverStateEnabled="true" :data-source="dataSource"
-                                :show-borders="true" key-expr="employeeId" @exporting="onExporting"
-                                :allow-column-reordering="move_column" :allow-column-resizing="colomn_resize"
-                                :column-auto-width="true">
-                                <DxScrolling column-rendering-mode="virtual" />
-                                <DxSearchPanel :visible="true" :highlight-case-sensitive="true" />
-                                <DxExport :enabled="true" :allow-export-selected-data="true" />
-                                <DxToolbar>
-                                    <DxItem location="after" template="pagination-table" />
-                                    <DxItem name="searchPanel" />
-                                    <DxItem name="exportButton" />
-                                    <DxItem name="groupPanel" />
-                                    <DxItem name="columnChooserButton" />
-                                </DxToolbar>
-                                <template #pagination-table>
-                                    <div v-if="rowTable > originData.rows">
-                                        <a-pagination v-model:current="originData.page"
-                                            v-model:page-size="originData.rows" :total="rowTable" show-less-items />
-                                    </div>
-                                </template>
-                                <DxColumn caption="성명 (상호)" cell-template="tag" />
-                                <template #tag="{ data }" class="custom-action">
-                                    <div class="custom-action">
-                                        <employee-info :idEmployee="data.data.employeeId" :name="data.data.name"
-                                            :idCardNumber="data.data.residentId" :status="data.data.status"
-                                            :foreigner="data.data.foreigner" :checkStatus="false" />
-                                    </div>
-                                </template>
-
-                                <DxColumn caption="주민등록번호" data-field="residentId" />
-                                <DxColumn caption="소득부분" cell-template="grade-cell" />
-                                <template #grade-cell="{ data }" class="custom-action">
-                                    <div class="custom-grade-cell">
-                                        <div class="custom-grade-cell-tag">{{ data.data.incomeTypeCode }}</div>
-                                        <span>{{ data.data.incomeTypeName }}</span>
-                                    </div>
-                                </template>
-
-                                <DxColumn :width="80" cell-template="pupop" />
-                                <template #pupop="{ data }" class="custom-action">
-                                    <div class="custom-action">
-                                        <a-space :size="10">
-                                            <a-tooltip placement="top">
-                                                <template #title>편집</template>
-                                                <EditOutlined />
-                                            </a-tooltip>
-                                            <a-tooltip placement="top" @click="modalHistory(data.data.employeeId)">
-                                                <template #title>변경이력</template>
-                                                <HistoryOutlined />
-                                            </a-tooltip>
-                                            <a-tooltip placement="top">
-                                                <template #title>변경이력</template>
-                                                <DeleteOutlined />
-                                            </a-tooltip>
-                                        </a-space>
-                                    </div>
-                                </template>
-                            </DxDataGrid>
-                            <div class="pagination-table" v-if="rowTable > originData.rows">
-                                <a-pagination v-model:current="originData.page" v-model:page-size="originData.rows"
-                                    :total="rowTable" show-less-items style="margin-top: 10px" @change="searching" />
+    <a-spin :spinning="loading" size="large">
+        <action-header title="거주자의 사업소득원천징수영수증 " @actionSearch="searching" />
+        <div id="pa-630">
+            <div class="search-form">
+                <a-row>
+                    <a-col :span="22">
+                        <a-row :gutter="[24, 8]">
+                            <a-col>
+                                <div class="dflex custom-flex global-year">
+                                    <label class="lable-item">귀속연도 :</label>
+                                    <a-tag color="#a3a2a0">귀 {{ globalYear }}</a-tag>
+                                </div>
+                            </a-col>
+                        </a-row>
+                    </a-col>
+                </a-row>
+            </div>
+            <div class="page-content">
+                <a-row style="margin-bottom: 20px;">
+                    <a-col :span="12">
+                        <div class="format-settings">
+                            <strong>서식 설정 : </strong>
+                            <div class="format-settings-text">
+                                <InfoCircleFilled /> 본 설정으로 적용된 서식으로 출력 및
+                                메일발송 됩니다.
                             </div>
-                            <PopupMessage :modalStatus="modalStatus" @closePopup="modalStatus = false"
-                                typeModal="confirm" title="Title Notification" content="Content notification" okText="네"
-                                cancelText="아니요" @checkConfirm="statusComfirm" />
-                            <HistoryPopup :modalStatus="modalHistoryStatus" @closePopup="modalHistoryStatus = false"
-                                :data="popupData" title="변경이력" typeHistory="pa-610" />
                         </div>
-                    </a-spin>
-                </a-col>
-            </a-row>
+                    </a-col>
+                    <a-col :span="12">
+                        <div class="created-date">
+                            <label class="lable-item">구분 :</label>
+                            <date-time-box width="150px" v-model:valueDate="valueDefaultIncomeBusiness.input.receiptDate"></date-time-box>
+                        </div>
+                    </a-col>
+                </a-row>
+                <a-row style="margin-bottom: 20px;">
+                    <a-col :span="24">
+                        <label class="lable-item">소득자보관용</label>
+                        <switch-basic style="width: 120px;" v-model:valueSwitch="valueSwitch" :textCheck="'소득자보관용'"
+                                :textUnCheck="'발행자 보관용'" />
+                    </a-col>
+                </a-row>
+                <DxDataGrid :show-row-lines="true" :hoverStateEnabled="true" :data-source="dataSource"
+                    :show-borders="true" key-expr="id" @exporting="onExporting" :allow-column-reordering="move_column"
+                    :allow-column-resizing="colomn_resize" :column-auto-width="true">
+                    <DxSearchPanel :visible="true" :highlight-case-sensitive="true" />
+                    <DxExport :enabled="true" :allow-export-selected-data="true" />
+                    <DxToolbar>
+                        <DxItem name="searchPanel" />
+                        <DxItem name="exportButton" />
+                        <DxItem name="groupPanel" />
+                        <DxItem name="addRowButton" show-text="always" />
+                        <DxItem name="columnChooserButton" />
+                    </DxToolbar>
+                    <DxColumn caption="성명 (상호)" cell-template="tag" />
+                    <template #tag="{ data }" class="custom-action">
+                        <div class="custom-action">
+                            <employee-info :idEmployee="data.data.employeeId" :name="data.data.name"
+                                :idCardNumber="data.data.residentId" :status="data.data.status"
+                                :foreigner="data.data.foreigner" :checkStatus="false" />
+                        </div>
+                    </template>
+                    <DxColumn caption="주민등록번호" data-field="residentId" />
+                    <DxColumn caption="소득부분" cell-template="grade-cell" />
+                    <template #grade-cell="{ data }" class="custom-action">
+                        <div class="custom-grade-cell">
+                            <div class="custom-grade-cell-tag">{{ data.data.incomeTypeCode }}</div>
+                            <span>{{ data.data.incomeTypeName }}</span>
+                        </div>
+                    </template>
+                    <DxColumn caption="지급총액" data-field="paymentAmount" />
+                    <DxColumn caption="원천징수세액 소득세" data-field="withholdingIncomeTax" />
+                    <DxColumn caption="원천징수세액 지방소득세" data-field="withholdingLocalIncomeTax" />
+                    <DxColumn caption="원천징수세액 계" data-field="withholdingRuralSpecialTax" />
+                    <DxSelection select-all-mode="allPages" show-check-boxes-mode="always" mode="multiple" />
+                </DxDataGrid>
+            </div>
         </div>
-    </div>
+    </a-spin>
 </template>
 <script lang="ts">
-import { defineComponent, ref, watch, reactive, computed } from "vue";
-import { useStore } from 'vuex';
-import { useQuery, useMutation } from "@vue/apollo-composable";
-import notification from "../../../../utils/notification";
-import queries from "../../../../graphql/queries/PA/PA6/PA610/index";
-import { DxDataGrid, DxColumn, DxPaging, DxExport, DxSelection, DxSearchPanel, DxToolbar, DxEditing, DxGrouping, DxScrolling, DxItem } from "devextreme-vue/data-grid";
-import { EditOutlined, HistoryOutlined, SearchOutlined, MenuFoldOutlined, MenuUnfoldOutlined, MailOutlined, PrinterOutlined, DeleteOutlined, SaveOutlined, InfoCircleFilled } from "@ant-design/icons-vue";
-import { onExportingCommon } from "../../../../helpers/commonFunction"
-import { origindata, ArrForeigner, valueDefaultAction } from "./utils";
-import DxButton from "devextreme-vue/button";
-import { companyId } from "../../../../../src/helpers/commonFunction";
-import dayjs from 'dayjs';
-import mutations from "../../../../graphql/mutations/PA/PA6/PA610/index";
-import HistoryPopup from '../../../../components/HistoryPopup.vue';
+import { ref, defineComponent, reactive, watch, computed } from "vue";
+import { useStore } from "vuex";
+import { useQuery } from "@vue/apollo-composable";
+import { InfoCircleFilled } from "@ant-design/icons-vue";
+import {
+    DxDataGrid,
+    DxColumn,
+    DxPaging,
+    DxExport,
+    DxSelection,
+    DxSearchPanel,
+    DxToolbar,
+    DxItem,
+} from "devextreme-vue/data-grid";
+import {
+    companyId,
+    onExportingCommon,
+} from "../../../../helpers/commonFunction";
+import queries from "../../../../graphql/queries/PA/PA6/PA630/index";
+
 export default defineComponent({
     components: {
         DxDataGrid,
@@ -114,114 +109,85 @@ export default defineComponent({
         DxSelection,
         DxExport,
         DxSearchPanel,
-        DxScrolling,
-        EditOutlined,
-        HistoryOutlined,
         DxToolbar,
-        DxEditing,
-        DxGrouping,
         DxItem,
-        SearchOutlined,
-        MenuFoldOutlined,
-        MenuUnfoldOutlined,
-        MailOutlined,
-        PrinterOutlined,
-        DeleteOutlined,
-        SaveOutlined,
-        ArrForeigner,
-        DxButton,
         InfoCircleFilled,
-        HistoryPopup
     },
     setup() {
-        let popupData = ref([])
-        let modalHistoryStatus = ref<boolean>(false)
-        const dataSource = ref([]);
+        const valueSwitch = ref(true);
         const store = useStore();
-        const per_page = computed(() => store.state.settings.per_page);
+
+        const globalYear = computed(() => store.state.settings.globalYear);
+        const trigger = ref<boolean>(true);
         const move_column = computed(() => store.state.settings.move_column);
         const colomn_resize = computed(() => store.state.settings.colomn_resize);
-        const rowTable = ref(0);
-        const originData = reactive({ ...origindata, rows: per_page });
-
-        const trigger = ref<boolean>(true);
-        const valueCallApiGetEmployeeBusinesses = reactive({
+        const dataSource = ref([]);
+        const originData = ref({
             companyId: companyId,
-            imputedYear: parseInt(dayjs().format('YYYY')),
-        })
-        let dataAction = reactive({
+            imputedYear: globalYear,
+        });
+        const valueDefaultIncomeBusiness = ref({
             companyId: companyId,
-            imputedYear: parseInt(dayjs().format('YYYY')),
-            employeeId: null,
-            incomeTypeCode: '940100',
             input: {
-                name: '',
-                foreigner: false,
-                nationality: '대한민국',
-                nationalityCode: 'KR',
-                stayQualification: 'C-4',
-                residentId: '',
-                incomeTypeName: '저술가',
-                email: '',
+                imputedYear: globalYear,
+                type: 1,
+                receiptDate: new Date().toJSON().slice(0, 10),
+            },
+            employeeKeys: {
+                employeeId: 0,
+                incomeTypeCode: ""
             }
-        })
-        const modalStatus = ref(false)
-        const textResidentId = ref('주민등록번호')
-        // ================GRAPQL==============================================
-        const { refetch: refetchData, loading: loadingGetEmployeeBusinesses, onError: errorGetEmployeeBusinesses, onResult: resEmployeeBusinesses } = useQuery(queries.getEmployeeBusinesses, valueCallApiGetEmployeeBusinesses, () => ({
+        });
+        const {
+            refetch: refetchData,
+            result,
+            loading,
+        } = useQuery(queries.searchIncomeBusinessWithholdingReceipts, originData, () => ({
             enabled: trigger.value,
             fetchPolicy: "no-cache",
         }));
-        resEmployeeBusinesses(res => {
-            dataSource.value = res.data.getEmployeeBusinesses
-        })
-        errorGetEmployeeBusinesses(res => {
-            notification('error', res.message)
-        })
-
-
-
-        // ================WATCHING============================================
-
-
-        // ================FUNCTION============================================
-        const onExporting = (e: any) => {
-            onExportingCommon(e.component, e.cancel, '영업자관리')
+        const onExporting = (e: { component: any; cancel: boolean }) => {
+            onExportingCommon(e.component, e.cancel, "계약정보관리&심사");
         };
+
+        watch(result, (value) => {
+            if (value) {
+                dataSource.value = value.searchIncomeBusinessWithholdingReceipts;
+                trigger.value = false;
+            }
+        });
+        watch(valueSwitch, (newValue) => {
+            if (newValue) {
+                valueDefaultIncomeBusiness.value.input.type = 1
+            } else {
+                valueDefaultIncomeBusiness.value.input.type = 2
+            }
+        });
+
         const searching = () => {
-            // originData.grade = saleGrade.value == 0 ? null : saleGrade.value;
-            // originData.statuses = [saleStatus.value];
-            // trigger.value = true;
-            // refetchData();
+            trigger.value = true;
+            refetchData();
         };
 
-        const modalHistory = (data: any) => {
-            modalHistoryStatus.value = true;
+        const sendMail = (sendType: string) => {
+            alert(sendType);
         }
-
-        const statusComfirm = (res:any)=>{
-            
-        }
-
         return {
-            textResidentId,
-            popupData,
-            modalHistoryStatus,
-            loadingGetEmployeeBusinesses,
-            rowTable,
-            dataSource,
-            per_page, move_column, colomn_resize,
-            originData,
-            dataAction,
-            modalStatus,
-            statusComfirm,
-            modalHistory,
-            onExporting,
+            valueDefaultIncomeBusiness,
+            valueSwitch,
+            loading,
             searching,
+            globalYear,
+            dataSource,
+            sendMail,
+            move_column,
+            colomn_resize,
+            onExporting,
         };
     },
 });
-</script>  
-<style scoped lang="scss" src="./style/style.scss" >
+</script>
+  
 
-</style>
+<style lang="scss" scoped src="./style/style.scss" />
+  
