22b27a2d647e09452cc5a904343aad8038aa5eb0
doankhiem <khiembi@townsoftvina.com>
2022-11-25 09:11:25 +0700

    create file api

---
+++ src/views/PA/PA2/PA220/utils/index.ts
@@ -0,0 +1,11 @@
+export const radioDivision = [
+    { id: 1, text: "전체" },
+    { id: 2, text: "계속" },
+    { id: 3, text: "중도" },
+]
+export const radioType = [
+    { id: 1, text: "소득자보관용" },
+    { id: 2, text: "소득자보관용" },
+    { id: 3, text: "발행자보관용" },
+    { id: 4, text: "발행자보고용" },
+]
