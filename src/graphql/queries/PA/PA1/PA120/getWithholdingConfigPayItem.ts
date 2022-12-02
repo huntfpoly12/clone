91d2a9415f55f86259fd5deb8b7337cfb0413fb5
Tran Mai Son <sontm@townsoftvina.com>
2022-12-02 09:43:18 +0700

    add Graph tab2

---
+++ src/graphql/queries/PA/PA1/PA120/getWithholdingConfigPayItem.ts
@@ -0,0 +1,35 @@
+import gql from "graphql-tag";
+export default gql`
+  query getWithholdingConfigPayItem(
+    $companyId: Int!
+    $imputedYear: Int!
+    $itemCode: Int!
+  ) {
+    getWithholdingConfigPayItem(
+      companyId: $companyId
+      imputedYear: $imputedYear
+      itemCode: $itemCode
+    ) {
+      itemCode
+      name
+      taxPayItemCode
+      taxPayItemName
+      taxfreePayItemCode
+      taxfreePayItemName
+      monthlyLimit
+      annualLimit
+      tax
+      taxFreeIncludeSubmission
+      use
+      sort
+      formula
+      createdAt
+      createdBy
+      updatedAt
+      updatedBy
+      ip
+      active
+      editable
+    }
+  }
+`;
