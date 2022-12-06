7764e8838f7fb4819a59081742ae217dcff1ef40
doankhiem <khiembi@townsoftvina.com>
2022-12-06 17:19:05 +0700

    update pa-510

--- src/views/PA/PA5/PA510/utils/index.ts
+++ src/views/PA/PA5/PA510/utils/index.ts
@@ -1,32 +1,32 @@
-export const radioCheckForeigner = [
-    { id: false, text: '내국인' },
-    { id: true, text: '외국인' }];
-export const radioCheckHouseholder = [
-    { id: 1, text: 'X' },
-    { id: 2, text: 'O' }];
-export const radioCheckPersenPension = [
-    { id: 1, text: '0%' },
-    { id: 2, text: '80%' }];
-export const radioCheckReductioRate = [
-    { id: 1, text: '50%' },
-    { id: 2, text: '70%' },
-    { id: 3, text: '90%' }
-];
-export const radioCheckReductionInput = [
-    { id: 1, text: '급여입력' },
-    { id: 2, text: '연말입력' },
-];
-export const IncomeTaxAppRate = [
-    { id: 1, text: '100%' },
-    { id: 2, text: '180%' },
-    { id: 3, text: '510%' },
-];
+export const sampleFormIncomeWageDaily = {
+    companyId: 29,
+    processKey: {
+        imputedYear: 2022,
+        imputedMonth: 12,
+        paymentYear: 2022,
+        paymentMonth: 12,
+    },
+    input: {
+        paymentDay: 1,
+        employeeId: 567567,
+        dailyWage: 1,
+        monthlyWage: 1,
+        workingDays: 1,
+        deductionItems: [],
+    }
 
-export const dataDemo = [
-    { id: 1, text: '100%' },
-    { id: 2, text: '180%' },
-    { id: 3, text: '510%' },
-];
+}
+export const sampleDataIncomeWageDaily = {
+    actualPayment: 0,
+    monthlyWage: 0,
+    dailyWage: 0,
+    workingDays: 0,
+    employee: {
+        name: '',
+        totalDeduction: 0,
+        monthlyPaycheck: true,
+    }
+}
 
 
 
