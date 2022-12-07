206ac51ff1c56c3d541c5c553691f00108ec5da8
dungtv <dungtv@townsoftvina.com>
2022-12-07 16:31:13 +0700

    edit 120

--- src/store/commonModule/index.ts
+++ src/store/commonModule/index.ts
@@ -1,16 +1,9 @@
 import getters from "./getters";
 import mutations from "./mutations";
-import {
-    DependantsRelation,
-    enum2Entries,
-} from "@bankda/jangbuda-common";
-var dependantsRelation = enum2Entries(DependantsRelation).map((value) => ({
-    value: value[1],
-    label: value[0],
-}));
+
 const commonModule = {
     state: () => ({
-        dependantsRelation
+        arrayRowedited : []
     }),
     getters,
     mutations,
