1669100d4f084c50d9b9f98a93b3b692199975a3
AnTus <tupa@townsoftvina.com>
2022-12-09 15:52:59 +0700

    update code page pa-620

--- src/graphql/mutations/PA/PA6/PA620/index.ts
+++ src/graphql/mutations/PA/PA6/PA620/index.ts
@@ -1,13 +1,15 @@
 
-import deleteEmployeeBusiness from "./deleteEmployeeBusiness";
+import deleteIncomeBusinesses from "./deleteIncomeBusinesses";
 import updateIncomeBusiness from "./updateIncomeBusiness";
 import createIncomeBusiness from "./createIncomeBusiness";
 import updateEmployeeBusiness from "./updateEmployeeBusiness";
+import changeIncomeBusinessPaymentDay from "./changeIncomeBusinessPaymentDay";
 const mutations = {
-    deleteEmployeeBusiness: deleteEmployeeBusiness,
+    deleteIncomeBusinesses: deleteIncomeBusinesses,
     createIncomeBusiness: createIncomeBusiness,
     updateIncomeBusiness: updateIncomeBusiness,
     updateEmployeeBusiness: updateEmployeeBusiness, 
+    changeIncomeBusinessPaymentDay: changeIncomeBusinessPaymentDay, 
 };
 
 export default mutations;
