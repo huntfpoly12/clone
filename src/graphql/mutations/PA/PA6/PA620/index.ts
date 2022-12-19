2808a851e070ddc7c7837d8f6925ea0566c4d207
dungtv <dungtv@townsoftvina.com>
2022-12-19 17:29:36 +0700

    edit 620

--- src/graphql/mutations/PA/PA6/PA620/index.ts
+++ src/graphql/mutations/PA/PA6/PA620/index.ts
@@ -1,17 +1,18 @@
-
 import deleteIncomeBusinesses from "./deleteIncomeBusinesses";
 import updateIncomeBusiness from "./updateIncomeBusiness";
 import createIncomeBusiness from "./createIncomeBusiness";
 import updateEmployeeBusiness from "./updateEmployeeBusiness";
 import changeIncomeBusinessPaymentDay from "./changeIncomeBusinessPaymentDay";
+import changeIncomeProcessBusinessStatus from "./changeIncomeProcessBusinessStatus";
 import copyIncomeBusinesses from "./copyIncomeBusinesses";
 const mutations = {
-    deleteIncomeBusinesses: deleteIncomeBusinesses,
-    createIncomeBusiness: createIncomeBusiness,
-    updateIncomeBusiness: updateIncomeBusiness,
-    updateEmployeeBusiness: updateEmployeeBusiness, 
-    changeIncomeBusinessPaymentDay: changeIncomeBusinessPaymentDay, 
-    copyIncomeBusinesses: copyIncomeBusinesses, 
+  deleteIncomeBusinesses: deleteIncomeBusinesses,
+  createIncomeBusiness: createIncomeBusiness,
+  updateIncomeBusiness: updateIncomeBusiness,
+  updateEmployeeBusiness: updateEmployeeBusiness,
+  changeIncomeBusinessPaymentDay: changeIncomeBusinessPaymentDay,
+  changeIncomeProcessBusinessStatus: changeIncomeProcessBusinessStatus,
+  copyIncomeBusinesses: copyIncomeBusinesses,
 };
 
 export default mutations;
--- src/store/commonModule/index.ts
+++ src/store/commonModule/index.ts
@@ -2,15 +2,17 @@ import getters from "./getters";
 import mutations from "./mutations";
 import actions from "./actions";
 import statesPA120 from "./screenStates/screenStatePA120";
-import statesPA510 from "./screenStates/screenStatePA510";
 import statesPA410 from "./screenStates/screenStatePA410";
+import statesPA510 from "./screenStates/screenStatePA510";
+import statesPA620 from "./screenStates/screenStatePA620";
 import layputState from "./screenStates/layoutState";
 const commonModule = {
     state: () => ({
         ...layputState,
         ...statesPA120,
+        ...statesPA410,
         ...statesPA510,
-        ...statesPA410
+        ...statesPA620,
     }),
     getters,
     mutations,
