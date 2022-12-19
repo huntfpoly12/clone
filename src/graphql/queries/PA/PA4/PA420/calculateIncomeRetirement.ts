aac363ab30f36daeb29f471c69e074621d016734
AnTus <tupa@townsoftvina.com>
2022-12-19 17:30:42 +0700

    update code

--- src/graphql/queries/PA/PA4/PA420/calculateIncomeRetirement.ts
+++
@@ -1,9 +0,0 @@
-import gql from "graphql-tag";
-export default gql`
-    query
-    calculateIncomeRetirement(
-            $companyId: Int!  
-            $input: IncomeRetirementCalculationInput! 
-        ){
-            calculateIncomeRetirement(companyId: $companyId, input: $input)  
-        }`
