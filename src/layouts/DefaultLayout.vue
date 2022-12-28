4a213b2ad17dade887c2423187e397e26c4da718
dungtv <dungtv@townsoftvina.com>
2022-12-28 10:11:49 +0700

    edit layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -142,7 +142,7 @@
           <a-layout-content
             :style="{ background: '#fff', margin: 0, minHeight: '280px' }"
           >
-            <div class="main-content">
+            <div class="main-content">{{ activeTab }}
               <template v-if="activeTab">      
                 <keep-alive :exclude="cachedTab">
              
@@ -298,6 +298,12 @@ export default defineComponent({
           }
           if (newValue.id.includes("bf-4")) {
             this.openKeys = ["bf-000", "bf-400"];
+          }
+          if (newValue.id.includes("bf-5")) {
+            this.openKeys = ["bf-000", "bf-500"];
+          }
+          if (newValue.id.includes("bf-6")) {
+            this.openKeys = ["bf-000", "bf-600"];
           }
           if (newValue.id.includes("cm-1")) {
             this.openKeys = ["cm-000", "cm-100"];
