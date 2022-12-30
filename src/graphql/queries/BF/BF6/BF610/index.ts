22db927ad8cb136f0333b400fa1dc9d8890886fe
AnTus <tupa@townsoftvina.com>
2022-12-30 17:03:02 +0700

    update code bf-610

--- src/graphql/queries/BF/BF6/BF610/index.ts
+++ src/graphql/queries/BF/BF6/BF610/index.ts
@@ -1,9 +1,11 @@
 import search from "./searchTaxWithholdingStatusReports";
 import getIncomProcessesInTaxWithholdingStatusReport from "./getIncomProcessesInTaxWithholdingStatusReport";
+import getTaxWithholdingStatusReportViewUrl from "./getTaxWithholdingStatusReportViewUrl";
 
 const queries = {
     search: search,
     getIncomProcessesInTaxWithholdingStatusReport: getIncomProcessesInTaxWithholdingStatusReport,
+    getTaxWithholdingStatusReportViewUrl: getTaxWithholdingStatusReportViewUrl,
 };
 
 export default queries;
