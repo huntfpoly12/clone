baba0b1ddf123a3e2d706aa8897958a9bd0386e4
dungtv1291 <112230409+dungtv1291@users.noreply.github.com>
2023-01-05 14:43:03 +0700

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,17 +3,11 @@ import { createStore } from "vuex";
 import authModule from "./authModule/index";
 import settingModule from "./settingModule/index";
 import commonModule from "./commonModule/index";
-import screen from "./screenModule/index";
-import PA720 from "./screenModule/PA720";
-import PA730 from "./screenModule/PA730";
 const store : any= createStore({
   modules: {
     auth: { ...authModule, namespaced: true },
     settings: { ...settingModule, namespaced: true },
-    common: { ...commonModule, namespaced: true},
-    PA720,
-    PA730,
-    // ...screen,
+    common: { ...commonModule, namespaced: true}
   },
 });
 export default store;
