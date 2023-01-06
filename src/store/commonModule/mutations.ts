fea1618293fc7a957c87896ad4ca37b7d8a0c869
truongnd <dinhtruong2808@gmail.com>
2023-01-06 10:58:08 +0700

    pa-120/debug-del-db

--- src/store/commonModule/mutations.ts
+++ src/store/commonModule/mutations.ts
@@ -1,9 +1,11 @@
 import screenMutatePA720 from "./screenMutates/screenMutatePA720.js";
+import screenMutatePA120 from "./screenMutates/screenMutatePA120.js";
 const mutations = {
     loadDataEmployee(state: any,dataEmployee :  any) {
         state.arrayEmployeePA410 = dataEmployee
     },
-    ...screenMutatePA720
+    ...screenMutatePA720,
+    ...screenMutatePA120,
 };
 
 export default mutations;
