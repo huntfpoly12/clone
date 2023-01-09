24011cbb8c9aa458ba66afbb7d57d370779383b2
AnTus <tupa@townsoftvina.com>
2023-01-09 17:06:10 +0700

    fix bug show confirm close popup when not edit value

--- src/graphql/queries/common/getSubscriptionRequestLogs.ts
+++ src/graphql/queries/common/getSubscriptionRequestLogs.ts
@@ -55,7 +55,7 @@ export default gql`
           bizNumber
           bizType
           residentId
-          decryptedResidentId
+          residentId 
           license {
             name
             url
