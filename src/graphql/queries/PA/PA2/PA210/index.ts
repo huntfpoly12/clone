45d64387e01bfec3e426a0f733df404014483583
doankhiem <khiembi@townsoftvina.com>
2023-01-10 10:44:36 +0700

    edit pa 210

--- src/graphql/queries/PA/PA2/PA210/index.ts
+++ src/graphql/queries/PA/PA2/PA210/index.ts
@@ -1,6 +1,7 @@
 import getTaxWithholdingStatusReports from "./getTaxWithholdingStatusReports"
-
+import getWithholdingConfig from "../../../CM/CM130/getWithholdingConfig"
 const queries = {
-    getTaxWithholdingStatusReports: getTaxWithholdingStatusReports
+    getTaxWithholdingStatusReports: getTaxWithholdingStatusReports,
+    getWithholdingConfig: getWithholdingConfig
 };
 export default queries;
