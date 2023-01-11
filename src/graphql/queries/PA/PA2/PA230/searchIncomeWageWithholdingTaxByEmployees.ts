d358762f478a9f64157e63c626713c35c88b84dd
AnTus <tupa@townsoftvina.com>
2023-01-11 10:17:58 +0700

    update page pa-230

---
+++ src/graphql/queries/PA/PA2/PA230/searchIncomeWageWithholdingTaxByEmployees.ts
@@ -0,0 +1,54 @@
+import gql from "graphql-tag";
+export default gql`
+query SearchIncomeWageWithholdingTaxByEmployees(
+    $companyId: Int!
+    $filter: IncomeWageWithholdingTaxByEmployeeSearchFilter!
+  ) {
+    searchIncomeWageWithholdingTaxByEmployees(
+      companyId: $companyId
+      filter: $filter
+    ) {
+      employeeId
+      totalPay
+      employee {
+        type
+        employeeId
+        incomeTypeCode
+        name
+        foreigner
+        nationality
+        nationalityCode
+        stayQualification
+        residentIdValidity
+        roadAddress
+        addressExtend 
+        department
+        responsibility
+        status 
+        residentId
+        joinedAt
+        leavedAt
+        president
+        householder
+        nationalPensionDeduction
+        healthInsuranceDeduction
+        longTermCareInsuranceDeduction
+        employeementInsuranceDeduction
+        insuranceSupport
+        nationalPensionSupportPercent
+        employeementInsuranceSupportPercent
+        employeementReduction
+        employeementReductionStartDate
+        employeementReductionFinishDate
+        employeementReductionRatePercent
+        employeementReductionInput
+        weeklyWorkingHours
+        deductionDependentCount
+        incomeTaxMagnification
+        incomePayment
+        totalPay
+        totalDeduction 
+      }
+    }
+  }
+  ` 
