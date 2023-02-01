56bbe68f69efde4b85fd14d0c4ee169a16c0b4f7
AnTus <tupa@townsoftvina.com>
2023-02-01 17:16:15 +0700

    update code

--- src/views/BF/BF6/BF640/components/Tab3Component.vue
+++ src/views/BF/BF6/BF640/components/Tab3Component.vue
@@ -3,7 +3,7 @@
         <a-row gutter="24" class="search-form-step-1 mt-10">
             <a-col>
                 <a-form-item label="신고구분" label-align="left">
-                    <electronic-filing-type-common />
+                    <electronic-filing-type />
                 </a-form-item>
             </a-col>
             <a-col>
