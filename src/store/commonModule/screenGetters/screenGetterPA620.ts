621637d64d2ffd9a7a5c9e19314ddeb8ccb822fb
truongnd <dinhtruong2808@gmail.com>
2023-02-16 14:56:36 +0700

    create/click-out fix/pa-620

--- src/store/commonModule/screenGetters/screenGetterPA620.ts
+++ src/store/commonModule/screenGetters/screenGetterPA620.ts
@@ -1,5 +1,6 @@
 const screenGetterPA620 = {
     processKeyPA620:(state:any)=> state.processKeyPA620,
+    paymentDayPA620:(state:any)=> state.paymentDayPA620,
 };
   
 export default screenGetterPA620;
