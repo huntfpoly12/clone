130209a9d9ffc846a3a4d96d8ad948f8d5e8ed0d
hieupv <hieu.pham@desports.gg>
2023-02-21 09:20:23 +0700

    create page pa-810
    fix types error
    
    fix types build

---
+++ src/graphql/queries/common/getCompany.ts
@@ -0,0 +1,75 @@
+import gql from "graphql-tag";
+export default gql`
+query getCompany ($id: Int!) {
+  getCompany (id: $id) {
+    id
+    code
+    name
+    bizNumber
+    bizType
+    address
+    phone
+    presidentName
+    presidentMobilePhone
+    residentId 
+    extendInfo{
+      detail{
+        name
+        zipcode
+        roadAddress
+        jibunAddress
+        addressExtend
+        addressDetail{
+          bcode
+          bname
+          buildingCode
+          buildingName
+          roadname
+          roadnameCode
+          sido
+          sigungu
+          sigunguCode
+          zonecode
+        }
+        phone
+        fax
+        licenseFileStorageId
+        license {
+            name
+            url
+        }
+      }
+      president{
+        name
+        birthday
+        mobilePhone
+        email
+      }
+      cmsBank{
+        bankType
+        accountNumber
+        ownerBizNumber
+        ownerName
+        withdrawDay
+      }
+    }
+    sealFileStorageId
+    createdAt
+    createdBy
+    updatedAt
+    updatedBy
+    ip
+    active
+    seal{
+      name
+      url
+      createdAt
+      createdBy
+      ip
+      active
+    }
+    canceledAt
+    unpaidMonths
+  }
+}
+`
