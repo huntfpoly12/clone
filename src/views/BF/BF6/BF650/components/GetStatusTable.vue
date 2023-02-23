13ea7e5d7b2f3106f062ad0921dcf36c40a66e16
doankhiem <khiembi@townsoftvina.com>
2023-02-23 10:58:52 +0700

    fix bf650 and bf610

--- src/views/BF/BF6/BF650/components/GetStatusTable.vue
+++ src/views/BF/BF6/BF650/components/GetStatusTable.vue
@@ -3,18 +3,26 @@
         <div @click="checkStatus(2) ? actionPopupProductionStatus() : ''">
             <production-status :typeTag="2" v-if="checkStatus(0)" padding="1px 10px" />
             <production-status :typeTag="3" v-if="checkStatus(1)" padding="1px 10px" />
-            <production-status :typeTag="4" v-if="checkStatus(2)" padding="1px 10px" />
-            <production-status :typeTag="5" v-if="checkStatus(-1)" padding="1px 10px" />
+
+            <a-tooltip placement="top" color="black" v-if="checkStatus(2)">
+                <template #title>전자신고파일 다운로드</template>
+                <span><production-status :typeTag="4" padding="1px 10px" /></span>
+            </a-tooltip>
+            <a-tooltip placement="top" color="black" v-if="checkStatus(-1)">
+                <template #title>{{ arrStatus[0].causeOfProductionFailure }}</template>
+                <span><production-status :typeTag="5" padding="1px 10px" /></span>
+            </a-tooltip>
         </div>
     </a-spin>
-    <PopupProductionStatus :modalStatus="modalConfirmProductionStatus"
-        @closePopup="modalConfirmProductionStatus = false" :data="arrStatus[0]" />
+    <PopupProductionStatus :modalStatus="modalConfirmProductionStatus" @closePopup="modalConfirmProductionStatus = false"
+        :data="arrStatus[0]" />
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import queries from "@/graphql/queries/BF/BF6/BF650/index";
 import { useQuery } from "@vue/apollo-composable";
 import PopupProductionStatus from "./PopupProductionStatus.vue"
+import notification from "@/utils/notification";
 export default defineComponent({
     components: {
         PopupProductionStatus
@@ -24,7 +32,8 @@ export default defineComponent({
     },
     setup(props, { emit }) {
         let modalConfirmProductionStatus = ref(false)
-        let arrStatus = ref([])
+        let arrStatus: any = ref([])
+        const trigger = ref(false);
         let dataSearch = ref({
             input: {
                 companyId: props.data?.companyId,
@@ -32,14 +41,26 @@ export default defineComponent({
                 paymentMonth: props.data?.paymentMonth,
             }
         })
+        watch(() => props.data, (newVal) => {
+            if (newVal) {
+                trigger.value = true;
+            }
+        }, { immediate: true })
         let {
-            onResult, loading
+            onResult, loading, onError,
         } = useQuery(queries.getElectronicFilingsByIncomeWageDailyPaymentStatement, dataSearch, () => ({
+            enabled: trigger.value,
             fetchPolicy: "no-cache"
         }));
         onResult((res: any) => {
-            arrStatus.value = res.data.getElectronicFilingsByIncomeWageDailyPaymentStatement
+            trigger.value = false;
+            let data = res.data.getElectronicFilingsByIncomeWageDailyPaymentStatement
+            if (data.length) {
+                arrStatus.value = data
+                emit('productionStatusData', data[0]);
+            }
         })
+        onError((res: any)=> {notification('error', res.message)})
         const checkStatus = (status: any) => {
             if (arrStatus.value.filter((val: any) => val.productionStatus == status).length != 0)
                 return true
--- src/views/BF/BF6/BF650/components/Tab1Component.vue
+++ src/views/BF/BF6/BF650/components/Tab1Component.vue
@@ -3,14 +3,14 @@
         <a-row gutter="24" class="search-form-step-1">
             <a-col>
                 <a-form-item label="지급연월">
-                    <month-picker-box-custom v-model:valueDate="datePayment" :minDate="new Date('2022-1-1')" bgColor="black"/>
+                    <month-picker-box-custom v-model:valueDate="datePayment" :minDate="new Date('2022-1-1')"
+                        bgColor="black" />
                 </a-form-item>
             </a-col>
             <a-col>
                 <a-form-item label="최종제작상태" label-align="left">
                     <div class="custom-note d-flex-center">
-                        <switch-basic v-model:valueSwitch="dataSearch.beforeProduction" textCheck="제작후"
-                            textUnCheck="제작전" />
+                        <switch-basic v-model:valueSwitch="dataSearch.beforeProduction" textCheck="제작후" textUnCheck="제작전" />
                         <div class="d-flex-center ml-5">
                             <img src="@/assets/images/iconInfo.png" style="width: 14px;" />
                             <span>제작전은 제작요청되지 않은 상태입니다.</span>
@@ -45,7 +45,8 @@
         <div class="title-table d-flex">
             <a-form-item label="파일 제작 설정" label-align="left">
                 <div class="custom-note d-flex-center">
-                    <switch-basic :disabled="true" v-model:valueSwitch="valueDefaultSwitch" textCheck="세무대리인신고" textUnCheck="납세자자진신고" />
+                    <switch-basic :disabled="true" v-model:valueSwitch="valueDefaultSwitch" textCheck="세무대리인신고"
+                        textUnCheck="납세자자진신고" />
                     <span class="d-flex-center">
                         <img src="@/assets/images/iconInfo.png" style="width: 16px;" />
                         <span class="pl-5">본 설정으로 적용된 파일로 다운로드 및 메일발송 됩니다.</span>
@@ -54,12 +55,12 @@
             </a-form-item>
             <a-form-item label="제출연월일" label-align="left">
                 <div class="d-flex-center">
-                    <date-time-box width="150px" dateFormat="YYYY-MM-DD" />
+                    <date-time-box width="150px" dateFormat="YYYY-MM-DD" v-model:valueDate="dateTime" />
                     <a-tooltip placement="topLeft" color="black">
                         <template #title>전자신고파일 제작 요청</template>
                         <div class="btn-modal-save" @click="openModalSave">
-                            <SaveOutlined class="fz-24 ml-5 action-save"/>
-                        <span style="margin-left: 5px;">파일제작요청</span>
+                            <SaveOutlined class="fz-24 ml-5 action-save" />
+                            <span style="margin-left: 5px;">파일제작요청</span>
                         </div>
                     </a-tooltip>
                 </div>
@@ -67,13 +68,12 @@
         </div>
         <div class="form-table">
             <a-spin :spinning="loadingTable">
-                <DxDataGrid :show-row-lines="true" :hoverStateEnabled="true" :data-source="dataSource"
-                    :show-borders="true" key-expr="companyId" class="mt-10" :allow-column-reordering="move_column"
-                    :allow-column-resizing="colomn_resize" :column-auto-width="true"
-                    @selection-changed="selectionChanged">
+                <DxDataGrid :show-row-lines="true" :hoverStateEnabled="true" :data-source="dataSource" :show-borders="true"
+                    key-expr="companyId" class="mt-10" :allow-column-reordering="move_column"
+                    :allow-column-resizing="colomn_resize" :column-auto-width="true" @selection-changed="selectionChanged">
                     <DxSelection mode="multiple" :fixed="true" />
                     <DxColumn caption="사업자코드" data-field="company.code" />
-                    <DxColumn caption="상호 주소" cell-template="company" />
+                    <DxColumn caption="상호-주소" cell-template="company" />
                     <template #company="{ data }">
                         {{ data.data.company.name }} - {{ data.data.company.address }}
                     </template>
@@ -91,18 +91,18 @@
                         format="yyyy-MM-dd hh:mm" />
                     <DxColumn caption="제작현황" cell-template="productionStatus" />
                     <template #productionStatus="{ data }">
-                        <GetStatusTable :data="data.data" />
+                        <GetStatusTable :data="data.data" @productionStatusData="productionStatusData" />
                     </template>
                     <DxSummary>
-                            <DxTotalItem column="사업자코드" summary-type="count" display-format="전체: {0}" />
-                            <!-- <DxTotalItem column="제작현황" :customize-text="customizeTotalMonthly" value-format="#,###" /> -->
-                            <DxTotalItem cssClass="custom-sumary" column="제작현황" :customize-text="customTextSummary" />
-                        </DxSummary>
+                        <DxTotalItem column="사업자코드" summary-type="count" display-format="전체: {0}" />
+                        <!-- <DxTotalItem column="제작현황" :customize-text="customizeTotalMonthly" value-format="#,###" /> -->
+                        <DxTotalItem cssClass="custom-sumary" column="제작현황" :customize-text="customTextSummary" />
+                    </DxSummary>
                 </DxDataGrid>
             </a-spin>
         </div>
     </div>
-    <PopupConfirmSave :modalStatus="modalConfirmMail" @closePopup="modalConfirmMail = false" :data="dataModalSave"/>
+    <PopupConfirmSave :modalStatus="modalConfirmMail" @closePopup="modalConfirmMail = false" :data="dataModalSave" />
 </template>
 <script lang="ts">
 import dayjs from "dayjs";
@@ -128,6 +128,7 @@ export default defineComponent({
     },
     setup(props) {
         let datePayment = ref(parseInt(dayjs().format('YYYYMM')))
+        let dateTime = ref(dayjs().endOf('month'))
         let valueDefaultSwitch = ref(true)
         let keySelect = ref([])
         let dataSearch: any = ref({ ...dataSearchUtils })
@@ -159,7 +160,7 @@ export default defineComponent({
         resTable((val: any) => {
             // Filtering the dataSource.value to remove duplicate data.
             let arrDataConvert: any = []
-            val.data.searchIncomeWageDailyPaymentStatementElectronicFilings.map((val: any) =>{
+            val.data.searchIncomeWageDailyPaymentStatementElectronicFilings.map((val: any) => {
                 let row = arrDataConvert.find((data: any) => data.companyId == val.companyId)
                 if (row) {
                     if (row.lastProductionRequestedAt < val.lastProductionRequestedAt) {
@@ -200,23 +201,53 @@ export default defineComponent({
             keySelect.value = res.selectedRowKeys
         }
         const customTextSummary = () => {
-            dataSource.value
-            return `제작전  제작대기  제작중  제작실패  제작성공 `
+            return `제작전: ${countStatus(productionStatusArr.value, 0, 'productionStatus')}, 제작대기: ${countStatus(productionStatusArr.value, 0, 'productionStatus')}, 제작중: ${countStatus(
+                productionStatusArr.value,
+                1, 'productionStatus'
+            )}, 제작실패: ${countStatus(productionStatusArr.value, -1, 'productionStatus')}, 제작성공: ${countStatus(productionStatusArr.value, 2, 'productionStatus')}`;
         }
+        let arr = ref<any>([])
+        let productionStatusArr = ref<any>([]);
+        const watchFirstRun = ref(true);
+        const productionStatusData = (emitVal: any) => {
+            arr.value.push(emitVal);
+            productionStatusArr.value = [emitVal];
+            reFreshDataGrid();
+        };
+        const countStatus = (arr: any[], type: number, propertyCompare: string) => {
+            if (Object.keys(arr).length === 0 || arr.length === 0) {
+                return 0;
+            }
+            let count = arr.reduce((acc: any, crr: any) => {
+                acc[crr[propertyCompare]] = acc[crr[propertyCompare]] ? acc[crr[propertyCompare]] + 1 : 1;
+                return acc;
+            }, {});
+            if (count[type]) {
+                return count[type];
+            }
+            return 0;
+        };
+        const reFreshDataGrid = () => {
+            if (watchFirstRun.value) {
+                dataSource.value = dataSource.value.concat([]);
+                dataSource.value = dataSource.value.splice(dataSource.value.length - 1, 1);
+                watchFirstRun.value = false;
+            }
+        };
         return {
             customTextSummary,
-            loadingTable, 
-            valueDefaultSwitch, 
-            datePayment, 
+            loadingTable,
+            valueDefaultSwitch,
+            datePayment,
             dataModalSave,
             // checkBoxSearch,
             productionStatusesCheckbox,
             dataSearch, dataSource, colomn_resize, move_column, modalConfirmMail,
-            selectionChanged, openModalSave
+            selectionChanged, openModalSave,
+            dateTime,
+            productionStatusData,
         }
     }
 })
 </script> 
-<style scoped lang="scss" src="../style/style.scss">
-
-</style>
+<style scoped lang="scss" src="../style/style.scss"></style>
