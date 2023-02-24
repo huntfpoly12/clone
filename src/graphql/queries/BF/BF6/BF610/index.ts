606bb49efdce167a00ffe4a510c71365ea9b1656
doankhiem <khiembi@townsoftvina.com>
2023-02-24 15:38:34 +0700

    new api bf 610

--- src/graphql/queries/BF/BF6/BF610/index.ts
+++ src/graphql/queries/BF/BF6/BF610/index.ts
@@ -1,10 +1,12 @@
 import search from "./searchTaxWithholdingStatusReports";
+import searchTaxWithholdingStatusReportsByImputedYearMonth from "./searchTaxWithholdingStatusReportsByImputedYearMonth";
 import getIncomProcessesInTaxWithholdingStatusReport from "./getIncomProcessesInTaxWithholdingStatusReport";
 import getTaxWithholdingStatusReportViewUrl from "./getTaxWithholdingStatusReportViewUrl";
 import sendTaxWithholdingStatusReportEmail from "./sendTaxWithholdingStatusReportEmail";
 
 const queries = {
     search: search,
+    searchTaxWithholdingStatusReportsByImputedYearMonth: searchTaxWithholdingStatusReportsByImputedYearMonth,
     getIncomProcessesInTaxWithholdingStatusReport: getIncomProcessesInTaxWithholdingStatusReport,
     getTaxWithholdingStatusReportViewUrl: getTaxWithholdingStatusReportViewUrl,
     sendTaxWithholdingStatusReportEmail: sendTaxWithholdingStatusReportEmail,
