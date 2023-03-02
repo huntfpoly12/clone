de9e8a19b80eb8d16eb13fbc0b6490cda09a5d13
dungtv <dungtv@townsoftvina.com>
2023-03-02 11:21:27 +0700

    fix bf 620, 630,640

--- src/components/common/ElectronicFilingType.vue
+++ src/components/common/ElectronicFilingType.vue
@@ -14,6 +14,10 @@ export default defineComponent({
             type: Number as PropType<number | null>,
             default: null,
         },
+        disabledList: {
+          type: Array,
+          default : []
+        }
     },
     setup(props, { emit }) {
         const value = ref(props.valueInput == 0 ? null : props.valueInput);
@@ -26,11 +30,13 @@ export default defineComponent({
         let options = ref([
             {
                 value: 1,
-                label: '원천세(원천징수이행상황신고서)'
+                label: '원천세(원천징수이행상황신고서)',
+                disabled: props.disabledList.includes(1),
             },
             {
                 value: 2,
                 label: '지방소득세',
+                disabled: props.disabledList.includes(2),
             },
             {
                 label: '지급명세서(연말정산)',
@@ -38,18 +44,22 @@ export default defineComponent({
                     {
                         value: 3,
                         label: '근로소득지급명세서',
+                        disabled: props.disabledList.includes(3),
                     },
                     {
                         value: 4,
                         label: '퇴직소득지급명세서',
+                        disabled: props.disabledList.includes(4),
                     },
                     {
                         value: 5,
                         label: '거주자기타소득지급명세서',
+                        disabled: props.disabledList.includes(5),
                     },
                     {
                         value: 6,
                         label: '거주자사업소득지급명세서',
+                        disabled: props.disabledList.includes(6),
                     },
                 ],
             },
@@ -58,15 +68,18 @@ export default defineComponent({
                 options: [
                     {
                         value: 7,
-                        label: '근로소득간이지급명세서',
+                      label: '근로소득간이지급명세서',
+                      disabled: props.disabledList.includes(7),
                     },
                     {
                         value: 8,
                         label: '거주자사업소득간이지급명세서',
+                        disabled: props.disabledList.includes(8),
                     },
                     {
                         value: 9,
                         label: '일용직근로소득지급명세서',
+                        disabled: props.disabledList.includes(9),
                     },
                 ],
             },
