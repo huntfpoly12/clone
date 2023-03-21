02ec4e467f3c853f78641630f422ebc6116bedb0
truongnd <dinhtruong2808@gmail.com>
2023-03-21 11:35:19 +0700

    pa-120/change-ui-tab2

--- src/views/PA/PA1/PA120/components/tab3Dependent/PopupUpdateDependent.vue
+++ src/views/PA/PA1/PA120/components/tab3Dependent/PopupUpdateDependent.vue
@@ -16,32 +16,32 @@
                                     :required="true" :itemSelected="itemSelected"></dependants-relation-select-box>
                             </a-form-item>
                             <a-form-item label="성명" label-align="right" class="red">
-                                <default-text-box placeholder="한글,영문(대문자) 입력 가능" width="200px" :required="true" :disabled="disabledButton"
+                                <default-text-box placeholder="한글,영문(대문자) 입력 가능" width="200px" :required="true" :disabled="disabledButton || formState.relation == 0"
                                     v-model:valueInput="formState.name"></default-text-box>
                             </a-form-item>
                             <a-form-item label="내/외국인" label-align="right" class="switchForeigner">
                                 <switch-basic textCheck="외국인" textUnCheck="내국인"
-                                    v-model:valueSwitch="formState.foreigner" :disabled="disabledButton"/>
+                                    v-model:valueSwitch="formState.foreigner" :disabled="disabledButton || formState.relation == 0"/>
                             </a-form-item>
                             <a-form-item :label="labelResidebId" label-align="right" class="red">
-                                <id-number-text-box :required="true" width="150px" :disabled="disabledButton"
+                                <id-number-text-box :required="true" width="150px" :disabled="disabledButton || formState.relation == 0"
                                     v-model:valueInput="formState.residentId">
                                 </id-number-text-box>
                             </a-form-item>
                             <a-form-item label="나이" label-align="right">
-                                <default-text-box width="200px" :disabled="true" v-model:valueInput="ageCount">
+                                <default-text-box width="200px" :disabled="true || formState.relation == 0" v-model:valueInput="ageCount">
                                 </default-text-box>
                             </a-form-item>
                             <a-form-item label="기본공제" label-align="right" class="red">
                                 <basic-deduction-select-box width="200px" v-model:valueInput="formState.basicDeduction"
-                                    :required="true" :disabled="disabledButton" :ageCount="ageCount"/>
+                                    :required="true" :disabled="disabledButton || formState.relation == 0" :ageCount="ageCount"/>
                             </a-form-item>
                             <a-form-item label="부녀자" label-align="right">
-                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.women" />
+                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.women"  :disabled="formState.relation == 0"/>
                             </a-form-item>
                             <a-form-item label="한부모" label-align="right">
                               <div class="input-text">
-                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.singleParent" />
+                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.singleParent" :disabled="formState.relation == 0" />
                                 <span style="color: #888888; font-size:11px">
                                   <img src="@/assets/images/iconInfo.png" style="width: 14px;" /> 부녀자 공제와 중복 공제 불가
                                 </span>
@@ -51,30 +51,30 @@
                         <a-col :span="12">
                             <a-form-item label="경로우대" label-align="right">
                               <div class="input-text">
-                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.senior" :disabled="isDisabledSenior"/>
+                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.senior" :disabled="isDisabledSenior || formState.relation == 0"/>
                                 <span style="color: #888888; font-size:11px">
                                   <img src="@/assets/images/iconInfo.png" style="width: 14px;" /> 만 70세 이상
                                 </span>
                               </div>
                             </a-form-item>
                             <a-form-item label="장애인" label-align="right">
-                                <disabled-type-radio-group v-model:valueRadioCheck="formState.disabled">
+                                <disabled-type-radio-group v-model:valueRadioCheck="formState.disabled" :disabled="formState.relation == 0">
                                 </disabled-type-radio-group>
                             </a-form-item>
                             <a-form-item label="출산입양" label-align="right">
-                                <maternity-adoption-radio-box v-model:valueRadioCheck="formState.maternityAdoption">
+                                <maternity-adoption-radio-box v-model:valueRadioCheck="formState.maternityAdoption" :disabled="formState.relation == 0">
                                 </maternity-adoption-radio-box>
                             </a-form-item>
                             <a-form-item label="자녀세액공제" label-align="right" class="d-flex-nowrap">
                               <div class="input-text long-text">
-                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.descendant" />
+                                <switch-basic textCheck="O" textUnCheck="X" v-model:valueSwitch="formState.descendant" :disabled="formState.relation == 0" />
                                 <span style="color: #888888; font-size:11px">
                                   <img src="@/assets/images/iconInfo.png" style="width: 14px;" /> 7세 이상 20세 이하의 자녀인 경우 공제 대상
                                 </span>
                               </div>
                             </a-form-item>
                             <a-form-item label="위탁관계" label-align="right">
-                                <default-text-box placeholder="최대 20자" width="200px" :maxCharacter="20" :disabled="consignDisabled"
+                                <default-text-box placeholder="최대 20자" width="200px" :maxCharacter="20" :disabled="consignDisabled || formState.relation == 0"
                                     v-model:valueInput="formState.consignmentRelationship"></default-text-box>
                             </a-form-item>
                             <!-- <a-form-item label="세대주여부" label-align="right">
@@ -88,7 +88,7 @@
                   <button-basic style="margin-right: 20px" text="삭제" mode="contained" :width="90"
                       :disabled="disabledButton || formState.relation == 0" @onClick="actionDeleteFuc($event)" />
                   <button-basic text="저장" type="default" mode="contained" :width="90"
-                      @onClick="actionUpdated($event)" />
+                      @onClick="actionUpdated($event)" :disabled="disabledButton || formState.relation == 0" />
                 </a-row>
               
                 <PopupMessage :modalStatus="modalStatusDelete" @closePopup="modalStatusDelete = false"
@@ -101,7 +101,7 @@
 <script lang="ts">
 
 import { defineComponent, reactive, ref, computed, watch, watchEffect } from "vue";
-import { useMutation, useQuery } from "@vue/apollo-composable";
+import { useMutation } from "@vue/apollo-composable";
 import { useStore } from "vuex";
 import mutations from "@/graphql/mutations/PA/PA1/PA120";
 import notification from "@/utils/notification";
@@ -109,7 +109,6 @@ import { companyId, convertAge } from "@/helpers/commonFunction";
 import {taxWaring} from '../../utils/index';
 import { h } from 'vue';
 const vnode = h('div', [h('div', '연말정산에 이미 반영된 경우, 삭제 후 연말정산 재정산해야 합니다'),h('div','그래도 삭제하시겠습니까?')])
-const contentDelete = '연말정산에 이미 반영된 경우, 삭제 후 연말정산 재정산해야 합니다.<br/> '
 export default defineComponent({
     components: {},
     props: {
@@ -296,7 +295,7 @@ export default defineComponent({
             formState,
             ageCount,
             disabledButton,convertAge,
-            setModalVisible, actionUpdated, statusComfirm, contentDelete,
+            setModalVisible, actionUpdated, statusComfirm,
             labelResidebId, actionDeleteFuc, modalStatusDelete,isDisabledSenior,itemSelected,consignDisabled,vnode
         };
     },
