d29ffe8c4d765a69acb4743c3d9ac2941f20cc20
kieuquan <kieuquan@townsoftvina.com>
2023-03-29 09:19:47 +0700

    bug 30 bf-630

---
+++ src/graphql/queries/AC/AC1/AC110/index.ts
@@ -0,0 +1,8 @@
+import getAccountingProcesses from './getAccountingProcesses';
+import getBankbookDetails from './getBankbookDetails';
+const queries = {
+   getAccountingProcesses,
+   getBankbookDetails
+};
+
+export default queries;
