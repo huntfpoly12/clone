efc625da135b509eedaa0f65722a8901fad42b9b
kieuquan <kieuquan@townsoftvina.com>
2023-04-20 10:29:46 +0700

    fix remove image ac-110

--- src/graphql/queries/AC/AC1/AC110/getBankbookDetailProofs.ts
+++ src/graphql/queries/AC/AC1/AC110/getBankbookDetailProofs.ts
@@ -2,6 +2,7 @@ import gql from "graphql-tag";
 export default gql`
     query getBankbookDetailProofs($companyId: Int!, $fiscalYear: Int!, $facilityBusinessId: Int!, $bankbookDetailDate: Int!, $bankbookDetailId: Int!) {
         getBankbookDetailProofs(companyId: $companyId, fiscalYear: $fiscalYear, facilityBusinessId: $facilityBusinessId, bankbookDetailDate: $bankbookDetailDate, bankbookDetailId: $bankbookDetailId) {
+          id
           name
           url
           turl
