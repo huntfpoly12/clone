fdbcf0f06587fbf1cce19d3c86cd2365b3f54538
doankhiem <khiembi@townsoftvina.com>
2023-04-24 17:23:30 +0700

    fix ac120

--- src/graphql/queries/AC/AC1/AC120/index.ts
+++ src/graphql/queries/AC/AC1/AC120/index.ts
@@ -2,11 +2,13 @@ import getAccountingProcesses from '../AC110/getAccountingProcesses';
 import getAccountingDocuments from './getAccountingDocuments'
 import searchSpendingAccountingDocuments from './searchSpendingAccountingDocuments'
 import getAccountingDocumentProofs from './getAccountingDocumentProofs'
+import getMyCompany from "@/graphql/queries/common/getMycompany";
 const queries = {
     getAccountingProcesses,
     getAccountingDocuments,
     searchSpendingAccountingDocuments,
     getAccountingDocumentProofs,
+    getMyCompany,
  };
  
  export default queries;
