9961beb9d3056726d5cebfe67eed63959a04bac4
truongnd <dinhtruong2808@gmail.com>
2023-04-28 15:17:06 +0700

    fix-pa/810-

--- src/views/BF/BF5/BF530/components/tab2/Tab2Component.vue
+++ src/views/BF/BF5/BF530/components/tab2/Tab2Component.vue
@@ -63,7 +63,7 @@
         <template #downA=" { data }: any " class="custom-action">
           <div class="d-flex justify-content-center">
             <DxButton type="ghost" class="" style="cursor: pointer" @click=" onGetAcquistionRp(data.data.workId) ">
-              <DownloadOutlined :size=" 12 " />
+              <DownloadOutlined :style="{fontSize: 12}"/>
             </DxButton>
           </div>
         </template>
@@ -81,7 +81,7 @@
         <template #downE=" { data } : any " class="custom-action">
           <div class="d-flex justify-content-center">
             <!-- <DxButton type="ghost" class="" style="cursor: pointer" @click=" onGetAcquistionRp(data.data.workId) ">
-              <DownloadOutlined :size=" 12 " />
+              <DownloadOutlined :style="{fontSize: 12}"/>
             </DxButton> -->
           </div>
         </template>
