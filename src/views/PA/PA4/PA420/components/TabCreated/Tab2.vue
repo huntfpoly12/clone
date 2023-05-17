0a68b8ad9f43f173afd2d26109e4b2d7ebf9bc61
huntfpoly12 <125422305+huntfpoly12@users.noreply.github.com>
2023-05-17 13:28:36 +0700

    fix bug 810 830 420 (#2896)
    Co-authored-by: huntfpoly12 <hieupv2412@gmail.com>

--- src/views/PA/PA4/PA420/components/TabCreated/Tab2.vue
+++ src/views/PA/PA4/PA420/components/TabCreated/Tab2.vue
@@ -29,7 +29,7 @@
         <div class="d-flex mt-20 mb-20 wf-100">
           <div class="d-flex-center" style="margin-left: 250px;">
             <button-basic text="퇴직금 계산" type="default" mode="contained" @onClick="calculateIncomeRetirement"/>
-            <info-tool-tip class="ml-5">
+            <info-tool-tip>
               상기 급여(수당)으로 퇴직금 계산합니다.
             </info-tool-tip>
           </div>
@@ -40,8 +40,8 @@
         <a-form-item label="퇴직급여(예상)">
           <div class="d-flex-center">
             <number-box-money v-model:valueInput="dataIncomeRetirement" :required="false" width="200px" :disabled="true" format="#0,###"/>
-            <span class="pl-5 mr-5">원</span>
-            <info-tool-tip class="ml-5">
+            <span class="pl-5">원</span>
+            <info-tool-tip>
               상기 급여(수당)으로 계산된 퇴직금으로 실제 지급된 퇴직금과는 상이할 수 있습니다.
             </info-tool-tip>
           </div>
@@ -52,8 +52,8 @@
                :class="definedRetirementBenefits != Number(dataIncomeRetirement) ? 'custom-input-number' : ''">
             <number-box-money :required="false" width="200px"
                               v-model:valueInput="definedRetirementBenefits" format="#0,###"/>
-            <span class="pl-5 mr-5">원</span>
-            <info-tool-tip class="ml-5">
+            <span class="pl-5">원</span>
+            <info-tool-tip>
               실제 지급된 퇴직급여를 입력합니다.
             </info-tool-tip>
           </div>
