401c866a6affffe8eaf8481b4763e299308ea291
DreamLee137 <124542297+DreamLee137@users.noreply.github.com>
2023-05-18 09:39:40 +0700

    Update message.ts

--- src/configs/enum/message.ts
+++ src/configs/enum/message.ts
@@ -139,7 +139,7 @@ export class Message extends BaseType {
   
   //과목전용조서
     public static AC570 = new Message({
-        '001': { "message" : "해당 과목전용조서 삭제시 전용일자 이후 등록된 예산서의 예산액이 틀려질 수도 있습니다. 그래도 삭제하시겠습니까?","yes": "네. 삭제합니","no":"아니요"}, // If you change the resolution type, all previously saved values ​​(including item details) are deleted and initialized. Would you like to proceed anyway?
+        '001': { "message" : "해당 과목전용조서 삭제시 전용일자 이후 등록된 예산서의 예산액이 틀려질 수도 있습니다. 그래도 삭제하시겠습니까?","yes": "네. 삭제합니다","no":"아니요"}, // If you change the resolution type, all previously saved values ​​(including item details) are deleted and initialized. Would you like to proceed anyway?
     });
   
   //후원자 관리
