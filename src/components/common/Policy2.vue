d2d3f58258c9b0a79f506d63a68a630ffaccec01
truongnd <dinhtruong2808@gmail.com>
2023-05-31 14:42:26 +0700

    fix-policy-lan-2

--- src/components/common/Policy2.vue
+++ src/components/common/Policy2.vue
@@ -1,75 +1,70 @@
 <template>
-
-
 <div class="policy-group" style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">
-    <p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:0in;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><strong><span style='font-size:16px;line-height:107%;font-family:"Malgun Gothic",sans-serif;background:yellow;'>2. 개인정보제공 및 활용동의</span></strong></p>
     <p>㈜장부다&nbsp;개인정보취급방침</p>
     <p>회사의&nbsp;개인정보취급방침의&nbsp;기본방향</p>
     <p>(주)장부다(이하&nbsp;&apos;회사&apos;라&nbsp;합니다.)는&nbsp;회원께서&nbsp;회원&nbsp;가입&nbsp;시에&nbsp;제공한&nbsp;개인정보와&nbsp;회원의&nbsp;서비스&nbsp;이용에서&nbsp;발생되는&nbsp;모든&nbsp;개인정보를&nbsp;매우&nbsp;중요하게&nbsp;생각하고&nbsp;있습니다.&nbsp;회원의&nbsp;개인정보는&nbsp;오직&nbsp;회사의&nbsp;서비스의&nbsp;제공을&nbsp;위해서만&nbsp;사용하며,&nbsp;개인정보가&nbsp;회원의&nbsp;동의&nbsp;없이&nbsp;유출되어&nbsp;피해를&nbsp;입지&nbsp;않도록,&nbsp;개인정보&nbsp;보호를&nbsp;위해&nbsp;최선을&nbsp;다할&nbsp;것입니다.&nbsp;아래는&nbsp;회원의&nbsp;개인정보보호와&nbsp;관련한&nbsp;회사의&nbsp;개인정보취급방침입니다.</p>
-    <p><strong>1조&nbsp;(총칙)</strong></p>
+    <p class="title-policy"><strong>1조&nbsp;(총칙)</strong></p>
     <ol start="1" type="1">
         <li>회사는&nbsp;회원의&nbsp;개인정보보호를&nbsp;매우&nbsp;중요시하며,&nbsp;정보통신망이용촉진등에&nbsp;관한&nbsp;법률,&nbsp;정보통신부가&nbsp;제정한&nbsp;개인정보보호지침&nbsp;등&nbsp;관계법령을&nbsp;준수합니다.&nbsp;회사는&nbsp;개인정보취급방침을&nbsp;통하여&nbsp;회원이&nbsp;제공하는&nbsp;개인정보가&nbsp;어떠한&nbsp;용도와&nbsp;방식으로&nbsp;이용되고&nbsp;있으며&nbsp;개인정보보호를&nbsp;위해&nbsp;어떠한&nbsp;조치가&nbsp;취해지고&nbsp;있는지&nbsp;알려드립니다.</li>
         <li>회사는&nbsp;개인정보취급방침을&nbsp;웹사이트&nbsp;페이지에&nbsp;공개함으로써&nbsp;회원이&nbsp;언제나&nbsp;용이하게&nbsp;볼&nbsp;수&nbsp;있도록&nbsp;조치합니다.</li>
         <li>회사의&nbsp;개인정보취급방침은&nbsp;정부의&nbsp;법률&nbsp;및&nbsp;지침에&nbsp;따라&nbsp;변경될&nbsp;수&nbsp;있으며&nbsp;회원은&nbsp;회사의&nbsp;홈페이지(URL :&nbsp;<a href="http://www.jangbuda.com/">www.jangbuda.com</a>)&nbsp;방문&nbsp;시&nbsp;수시로&nbsp;그&nbsp;내용을&nbsp;확인할&nbsp;수&nbsp;있습니다.</li>
     </ol>
-    <p><strong>2조&nbsp;(개인정보 수집에 대한 동의)&nbsp;</strong></p>
+    <p class="title-policy"><strong>2조&nbsp;(개인정보 수집에 대한 동의)&nbsp;</strong></p>
     <p>회사는&nbsp;회원이&nbsp;회사의&nbsp;개인정보취급방침&nbsp;또는&nbsp;이용약관의&nbsp;내용에&nbsp;대해&nbsp;&quot;동의합니다&quot;버튼&nbsp;또는&nbsp;&quot;동의하지&nbsp;않습니다&quot;버튼을&nbsp;클릭할&nbsp;수&nbsp;있는&nbsp;절차를&nbsp;마련하며, &quot;동의합니다&quot;버튼을&nbsp;클릭하면&nbsp;개인정보&nbsp;수집에&nbsp;대해&nbsp;동의한&nbsp;것으로&nbsp;봅니다.</p>
-    <p><strong>3조&nbsp;(개인정보 수집 항목)</strong></p>
+    <p class="title-policy"><strong>3조&nbsp;(개인정보 수집 항목)</strong></p>
     <ol start="1" type="1">
         <li>회사가&nbsp;회원을&nbsp;대상으로&nbsp;각종&nbsp;서비스를&nbsp;제공하기&nbsp;위해&nbsp;수집하는&nbsp;필수&nbsp;회원정보는&nbsp;다음과&nbsp;같습니다.&nbsp;①&nbsp;이용자명&nbsp;②&nbsp;생년월일&nbsp;또는&nbsp;주민등록번호&nbsp;또는&nbsp;사업자번호&nbsp;③&nbsp;로그인&nbsp;ID&nbsp;및&nbsp;비밀번호&nbsp;④&nbsp;금융기관,&nbsp;계좌번호&nbsp;및&nbsp;비밀번호,&nbsp;인터넷뱅킹&nbsp;아이디&nbsp;및&nbsp;비밀번호&nbsp;⑤&nbsp;주소&nbsp;⑥&nbsp;연락처&nbsp;⑦&nbsp;이메일&nbsp;⑧&nbsp;서비스&nbsp;이용기록&nbsp;⑨&nbsp;접속로그</li>
         <li>제1항의&nbsp;필수&nbsp;회원&nbsp;정보&nbsp;항목&nbsp;이외에&nbsp;회원가입&nbsp;시&nbsp;회원이&nbsp;원할&nbsp;경우에&nbsp;추가&nbsp;정보를&nbsp;선택하여&nbsp;제공하실&nbsp;수&nbsp;있도록&nbsp;되어&nbsp;있으며&nbsp;일부&nbsp;서비스에&nbsp;대한&nbsp;신청&nbsp;및&nbsp;접수&nbsp;시에&nbsp;회원이&nbsp;원하는&nbsp;정확한&nbsp;서비스&nbsp;내용을&nbsp;파악하여&nbsp;계약&nbsp;및&nbsp;결재를&nbsp;원활하게&nbsp;하기&nbsp;위하여&nbsp;추가적인&nbsp;정보를&nbsp;요구할&nbsp;수&nbsp;있습니다.</li>
         <li>필수&nbsp;회원정보&nbsp;중&nbsp;e-mail&nbsp;주소는&nbsp;서비스와&nbsp;관련된&nbsp;통지의&nbsp;수단&nbsp;이외에&nbsp;홍보성&nbsp;메일이나&nbsp;정기적ㆍ비정기적&nbsp;정보&nbsp;제공의&nbsp;수단으로&nbsp;사용될&nbsp;수&nbsp;있습니다.&nbsp;단&nbsp;회원이&nbsp;원하지&nbsp;않을&nbsp;경우&nbsp;회사는&nbsp;해당&nbsp;회원을&nbsp;e-mail&nbsp;대량발송의&nbsp;대상으로부터&nbsp;제외합니다.</li>
         <li>회사에&nbsp;광고를&nbsp;게재하는&nbsp;광고주나&nbsp;서비스를&nbsp;제휴하여&nbsp;링크&nbsp;되어있는&nbsp;사이트에서&nbsp;별도로&nbsp;개인정보를&nbsp;수집할&nbsp;경우,&nbsp;개인정보를&nbsp;수집하는&nbsp;행위에&nbsp;대해서는&nbsp;본&nbsp;개인정보취급방침이&nbsp;적용되지&nbsp;않음을&nbsp;알려드립니다.</li>
     </ol>
-    <p><strong>4조&nbsp;(개인정보의 수집목적 및 이용목적)</strong></p>
+    <p class="title-policy"><strong>4조&nbsp;(개인정보의 수집목적 및 이용목적)</strong></p>
     <ol start="1" type="1">
         <li>회사는&nbsp;다음과&nbsp;같은&nbsp;목적을&nbsp;위하여&nbsp;개인정보를&nbsp;수집합니다.&nbsp;①&nbsp;회사의&nbsp;서비스&nbsp;제공&nbsp;②&nbsp;인터넷&nbsp;금융상담&nbsp;서비스&nbsp;제공&nbsp;③&nbsp;인터넷&nbsp;금융상품&nbsp;중개서비스&nbsp;제공</li>
         <li>회원의&nbsp;개인정보는&nbsp;동조&nbsp;제1항에서&nbsp;밝힌&nbsp;목적&nbsp;이외에는&nbsp;사용하지&nbsp;않습니다.</li>
     </ol>
-    <p><strong>5조&nbsp;(개인정보의 공유 및 제공)</strong></p>
+    <p class="title-policy"><strong>5조&nbsp;(개인정보의 공유 및 제공)</strong></p>
     <ol start="1" type="1">
         <li>회사는&nbsp;회원의&nbsp;개인정보를&nbsp;제4조&nbsp;&apos;개인정보의&nbsp;수집목적&nbsp;및&nbsp;이용목적&apos;과&nbsp;무관한&nbsp;제&nbsp;3자에게&nbsp;제공하지&nbsp;않습니다.&nbsp;단,&nbsp;다음&nbsp;각&nbsp;호의&nbsp;1에&nbsp;해당하는&nbsp;경에는&nbsp;예외로&nbsp;합니다.&nbsp;①&nbsp;회사의&nbsp;이용자에&nbsp;대한&nbsp;당해&nbsp;서비스&nbsp;제공에&nbsp;필요한&nbsp;경우&nbsp;②&nbsp;금융실명거래및비밀보장에관한법률,&nbsp;신용정보의이용및보호에관한법률,&nbsp;전기통신기본법,&nbsp;전기통신사업법,&nbsp;지방세법,&nbsp;소비자보호법,&nbsp;한국은행법,&nbsp;형사소송법&nbsp;등&nbsp;관계법령에&nbsp;특별한&nbsp;규정이&nbsp;있는&nbsp;경우&nbsp;③&nbsp;서비스&nbsp;제공에&nbsp;따른&nbsp;요금&nbsp;정산을&nbsp;위하여&nbsp;필요한&nbsp;경우&nbsp;④&nbsp;관계법령이&nbsp;허용하는&nbsp;한도&nbsp;내에서&nbsp;회사의&nbsp;계열회사&nbsp;또는&nbsp;제휴사에&nbsp;제공하는&nbsp;경우&nbsp;⑤&nbsp;통계작성,&nbsp;학술연구&nbsp;또는&nbsp;시장조사를&nbsp;위하여&nbsp;필요한&nbsp;경우로서&nbsp;특정&nbsp;개인을&nbsp;식별할&nbsp;수&nbsp;없는&nbsp;형태로&nbsp;제공하는&nbsp;경우</li>
         <li>회사는&nbsp;제&nbsp;4조&nbsp;&apos;개인정보의&nbsp;수집목적&nbsp;및&nbsp;이용목적&apos;의&nbsp;범위&nbsp;안에서&nbsp;회원에게&nbsp;보다&nbsp;나은&nbsp;서비스를&nbsp;제공하기&nbsp;위하여&nbsp;회원의&nbsp;동의를&nbsp;받아&nbsp;회원의&nbsp;개인정보를&nbsp;제휴사에&nbsp;제공하거나&nbsp;제휴사와&nbsp;공유할&nbsp;수&nbsp;있습니다.</li>
     </ol>
-    <p><strong>6조&nbsp;(개인정보의 열람&middot;정정 및 관리)</strong></p>
+    <p class="title-policy"><strong>6조&nbsp;(개인정보의 열람&middot;정정 및 관리)</strong></p>
     <ol start="1" type="1">
         <li>회원은&nbsp;언제든지&nbsp;등록되어&nbsp;있는&nbsp;회원의&nbsp;개인정보를&nbsp;열람하거나&nbsp;정정하실&nbsp;수&nbsp;있습니다.&nbsp;개인정보&nbsp;열람&nbsp;및&nbsp;정정을&nbsp;하고자&nbsp;할&nbsp;경우에는&nbsp;회사의&nbsp;사이트에&nbsp;로그인&nbsp;하신&nbsp;후&nbsp;&apos;회원가입/정보변경&apos;&nbsp;메뉴에서&nbsp;열람&nbsp;또는&nbsp;정정하거나,&nbsp;개인정보&nbsp;관리책임자에게&nbsp;e-mail로&nbsp;연락하시면&nbsp;됩니다.</li>
         <li>회원이&nbsp;사용하시고&nbsp;있는&nbsp;비밀번호는&nbsp;원칙적으로&nbsp;회원&nbsp;본인만이&nbsp;사용하실&nbsp;수&nbsp;있으며,&nbsp;회사의&nbsp;고의&nbsp;또는&nbsp;중대한&nbsp;과실이&nbsp;없는&nbsp;경우에는&nbsp;개인의&nbsp;비밀번호&nbsp;도용&nbsp;또는&nbsp;유출에&nbsp;대하여&nbsp;회사는&nbsp;책임지지&nbsp;않습니다.</li>
     </ol>
-    <p><strong>7조&nbsp;(개인정보 수집, 이용, 제공에 대한 동의 철회)</strong></p>
+    <p class="title-policy"><strong>7조&nbsp;(개인정보 수집, 이용, 제공에 대한 동의 철회)</strong></p>
     <p>회원가입&nbsp;등을&nbsp;통해&nbsp;개인정보의&nbsp;수집,&nbsp;이용,&nbsp;제공에&nbsp;대해&nbsp;회원께서&nbsp;동의하신&nbsp;내용을&nbsp;회원은&nbsp;언제든지&nbsp;철회하실&nbsp;수&nbsp;있습니다.&nbsp;동의철회를&nbsp;하고자&nbsp;하시는&nbsp;경우에는&nbsp;홈페이지의&nbsp;&apos;회원가입/정보변경&apos;&nbsp;메뉴에서&nbsp;회원탈퇴를&nbsp;하시거나&nbsp;개인정보&nbsp;관리책임자에게&nbsp;e-mail&nbsp;등으로&nbsp;연락하시면&nbsp;됩니다.</p>
-    <p><strong>8조&nbsp;(개인정보의 보유 및 이용기간)</strong></p>
+    <p class="title-policy"><strong>8조&nbsp;(개인정보의 보유 및 이용기간)</strong></p>
     <ol start="1" type="1">
         <li>원칙적으로,&nbsp;개인정보&nbsp;수집&nbsp;및&nbsp;이용목적이&nbsp;달성된&nbsp;후에는&nbsp;해당&nbsp;정보를&nbsp;지체&nbsp;없이&nbsp;파기합니다.&nbsp;단,&nbsp;다음의&nbsp;정보에&nbsp;대해서는&nbsp;아래의&nbsp;이유로&nbsp;명시한&nbsp;기간&nbsp;동안&nbsp;보존합니다.&nbsp;①&nbsp;보존&nbsp;항목&nbsp;:&nbsp;이용자명,&nbsp;생년월일&nbsp;또는&nbsp;사업자번호,&nbsp;로그인IDo비밀번호,&nbsp;계좌&nbsp;번호o비밀번호,&nbsp;인터넷뱅킹&nbsp;아이디o비밀번호,&nbsp;연락처,&nbsp;이메일,&nbsp;서비스&nbsp;이용기록,&nbsp;접속로그&nbsp;②&nbsp;보존&nbsp;근거&nbsp;:&nbsp;서비스&nbsp;이용의&nbsp;혼선방지,&nbsp;분쟁해결&nbsp;및&nbsp;수사기관의&nbsp;요청에&nbsp;따른&nbsp;협조,&nbsp;서비스의&nbsp;부정&nbsp;및&nbsp;불량&nbsp;이용&nbsp;방지&nbsp;및&nbsp;부정/불량이용자의&nbsp;재가입&nbsp;방지&nbsp;③&nbsp;보존&nbsp;기간&nbsp;: 1년</li>
         <li>관계법령의&nbsp;규정에&nbsp;의하여&nbsp;보존할&nbsp;필요가&nbsp;있는&nbsp;경우&nbsp;회사는&nbsp;아래와&nbsp;같이&nbsp;관계법령에서&nbsp;정한&nbsp;일정한&nbsp;기간&nbsp;동안&nbsp;회원정보를&nbsp;보관합니다.&nbsp;①&nbsp;보존&nbsp;항목&nbsp;:&nbsp;결제기록&nbsp;②&nbsp;보존&nbsp;근거&nbsp;:&nbsp;전자상거래등에서의&nbsp;소비자보호에&nbsp;관한&nbsp;법률&nbsp;③&nbsp;보존&nbsp;기간&nbsp;: 5년</li>
     </ol>
-    <p><strong>9조&nbsp;(개인정보보호를 위한 기술적 대책)&nbsp;</strong></p>
+    <p class="title-policy"><strong>9조&nbsp;(개인정보보호를 위한 기술적 대책)&nbsp;</strong></p>
     <p>회사는&nbsp;회원의&nbsp;개인정보를&nbsp;취급함에&nbsp;있어&nbsp;개인정보가&nbsp;분실,&nbsp;도난,&nbsp;누출,&nbsp;변조&nbsp;또는&nbsp;훼손되지&nbsp;않도록&nbsp;안전성&nbsp;확보를&nbsp;위하여&nbsp;다음과&nbsp;같은&nbsp;기술적&nbsp;대책을&nbsp;가지고&nbsp;있습니다.</p>
     <ol start="1" type="1">
         <li>회원의&nbsp;개인정보는&nbsp;비밀번호에&nbsp;의해&nbsp;보호되며,&nbsp;파일&nbsp;및&nbsp;전송&nbsp;데이터를&nbsp;암호화하거나&nbsp;파일&nbsp;잠금&nbsp;기능을&nbsp;사용</li>
         <li>암호&nbsp;알고리즘을&nbsp;이용하여&nbsp;네트워크&nbsp;상의&nbsp;개인정보를&nbsp;안전하게&nbsp;전송할&nbsp;수&nbsp;있는&nbsp;보안장치를&nbsp;채택</li>
         <li>해킹&nbsp;등에&nbsp;의해&nbsp;회원의&nbsp;개인정보가&nbsp;유출되는&nbsp;것을&nbsp;방지하기&nbsp;위해,&nbsp;외부로부터의&nbsp;침입을&nbsp;차단하는&nbsp;장치를&nbsp;이용</li>
     </ol>
-    <p><strong>10조&nbsp;(개인정보보호를 위한 관리적 대책)</strong></p>
+    <p class="title-policy"><strong>10조&nbsp;(개인정보보호를 위한 관리적 대책)</strong></p>
     <ol start="1" type="1">
         <li>회사는&nbsp;회원의&nbsp;개인정보를&nbsp;취급할&nbsp;수&nbsp;있는&nbsp;인력을&nbsp;최소한으로&nbsp;한정하고&nbsp;해당&nbsp;인원에게&nbsp;정기적,&nbsp;비정기적&nbsp;보안&nbsp;교육을&nbsp;실시합니다.</li>
         <li>회사는&nbsp;개인정보를&nbsp;처리하는&nbsp;시스템의&nbsp;사용자를&nbsp;지정하여&nbsp;사용자&nbsp;비밀번호를&nbsp;부여하고&nbsp;이를&nbsp;정기적으로&nbsp;갱신합니다.</li>
     </ol>
-    <p><strong>11조&nbsp;(개인정보의 위탁처리)</strong></p>
+    <p class="title-policy"><strong>11조&nbsp;(개인정보의 위탁처리)</strong></p>
     <ol start="1" type="1">
         <li>회사는&nbsp;서비스&nbsp;향상을&nbsp;위해서&nbsp;회원의&nbsp;개인정보를&nbsp;외부에&nbsp;위탁하여&nbsp;처리할&nbsp;수&nbsp;있습니다.&nbsp;이&nbsp;경우에는&nbsp;미리&nbsp;그&nbsp;사실을&nbsp;회원에게&nbsp;고지합니다.</li>
         <li>개인정보의&nbsp;처리를&nbsp;위탁하는&nbsp;경우에는&nbsp;위탁계약&nbsp;등을&nbsp;통하여&nbsp;서비스&nbsp;제공자의&nbsp;개인정보보호&nbsp;관련&nbsp;지시엄수,&nbsp;개인정보에&nbsp;관한&nbsp;비밀유지,&nbsp;제3자&nbsp;제공의&nbsp;금지&nbsp;및&nbsp;사고시의&nbsp;책임부담&nbsp;등을&nbsp;명확히&nbsp;규정하고&nbsp;당해&nbsp;계약내용을&nbsp;서면&nbsp;또는&nbsp;전자적으로&nbsp;보관합니다.</li>
     </ol>
-    <p><strong>12조&nbsp;(쿠키의 사용)&nbsp;</strong></p>
+    <p class="title-policy"><strong>12조&nbsp;(쿠키의 사용)&nbsp;</strong></p>
     <p>회사는&nbsp;회원인증을&nbsp;위하여&nbsp;쿠키(cookie)를&nbsp;사용합니다.&nbsp;쿠키는&nbsp;로그아웃&nbsp;시에&nbsp;자동으로&nbsp;PC에&nbsp;저장되지&nbsp;않고&nbsp;삭제되도록&nbsp;되어있으므로&nbsp;공공장소나&nbsp;타인이&nbsp;사용할&nbsp;수&nbsp;있는&nbsp;PC를&nbsp;사용하실&nbsp;경우에는&nbsp;로그인&nbsp;후&nbsp;서비스&nbsp;이용이&nbsp;끝나시면&nbsp;반드시&nbsp;로그아웃을&nbsp;해&nbsp;주시기&nbsp;바랍니다.&nbsp;단,&nbsp;회원이&nbsp;사용하는&nbsp;브라우저의&nbsp;설정&nbsp;변경에&nbsp;의해&nbsp;쿠키의&nbsp;사용을&nbsp;거부할&nbsp;수&nbsp;있으나&nbsp;이&nbsp;경우&nbsp;사이트&nbsp;이용에&nbsp;여러&nbsp;가지&nbsp;불편이&nbsp;있을&nbsp;수&nbsp;있습니다.</p>
-    <p><strong>13조&nbsp;(개인정보 관리책임자)&nbsp;</strong></p>
+    <p class="title-policy"><strong>13조&nbsp;(개인정보 관리책임자)&nbsp;</strong></p>
     <p>회사는&nbsp;개인정보에&nbsp;대한&nbsp;의견수렴&nbsp;및&nbsp;불만처리를&nbsp;담당하는&nbsp;개인정보&nbsp;관리책임자를&nbsp;지정하고&nbsp;있습니다.&nbsp;개인정보&nbsp;관리책임자는&nbsp;다음과&nbsp;같습니다.&nbsp;성&nbsp;명:&nbsp;강헌&nbsp;소속:&nbsp;경영지원/대표이사&nbsp;전화번호: (02)3663-3247 e-mail:&nbsp;<a href="mailto:hkang@jangbuda.com">hkang@jangbuda.com</a></p>
-    <p><strong>14조&nbsp;(의견수렴 및 불만처리)&nbsp;</strong></p>
+    <p class="title-policy"><strong>14조&nbsp;(의견수렴 및 불만처리)&nbsp;</strong></p>
     <p>회사는&nbsp;개인정보보호와&nbsp;관련하여&nbsp;회원이&nbsp;의견과&nbsp;불만을&nbsp;제기할&nbsp;수&nbsp;있는&nbsp;창구를&nbsp;개설하고&nbsp;있습니다.&nbsp;개인정보보호와&nbsp;관련한&nbsp;불만이&nbsp;있으신&nbsp;분은&nbsp;제13조의&nbsp;개인정보&nbsp;관리책임자에게&nbsp;의견을&nbsp;주시면&nbsp;접수&nbsp;즉시&nbsp;조치하여&nbsp;처리결과를&nbsp;통보해&nbsp;드립니다.</p>
     <p>(부칙)</p>
-    <ol>
-        <li>(시행일)&nbsp;본&nbsp;개인정보취급방침은&nbsp;2023년&nbsp;06월&nbsp;01일부터&nbsp;적용됩니다.&nbsp;</li>
-    </ol>
+    <p>I. (시행일)&nbsp;본&nbsp;개인정보취급방침은&nbsp;2023년&nbsp;06월&nbsp;01일부터&nbsp;적용됩니다.&nbsp;</p>
     <p>II. 기타 상세사항은 고객만족센터((02)3663-3247~8)로 문의 바랍니다.</p>
   </div>
 
