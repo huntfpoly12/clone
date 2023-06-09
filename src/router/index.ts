686af1ac8d697f867f198f8ba13765dbf363a9ea
dungtv1291 <dungtv@townsoftvina.com>
2023-06-09 15:40:10 +0700

    fix 320

--- src/router/index.ts
+++ src/router/index.ts
@@ -463,9 +463,6 @@ router.beforeEach((to, from, next) => {
   const token = sessionStorage.getItem("token");
 
   const { read } = useCheckPermission(roles)
-  console.log(read, 'sfdsfdsff')
-  console.log(token, 'sfdsfdsff')
-  console.log(requiresAuth ,'sfdsfdsff')
   // check if token isExpired
   if (token && getJwtObject(token).isExpired()) {
     store.dispatch('auth/checkToken')
