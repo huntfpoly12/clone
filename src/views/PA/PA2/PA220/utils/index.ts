9a9d5fbd680e2243186af9934aa966e555d93eb9
doankhiem <khiembi@townsoftvina.com>
2023-06-17 10:18:56 +0700

    fix search name and css

--- src/views/PA/PA2/PA220/utils/index.ts
+++ src/views/PA/PA2/PA220/utils/index.ts
@@ -1,7 +1,7 @@
 export const radioLeaved = [
     { id: 1, text: "전체", value: null },
-    { id: 2, text: "계속", value: true },
-    { id: 3, text: "중도", value: false },
+    { id: 2, text: "계속", value: false },
+    { id: 3, text: "중도", value: true },
 ]
 export const radioType = [
     { id: 1, text: "소득자보관용", value: 1 },
