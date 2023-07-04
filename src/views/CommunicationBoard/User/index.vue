61f1695e4b1f2ecd74a0ac3b1a1ac02e5008bf5e
doankhiem <khiembi@townsoftvina.com>
2023-07-04 14:41:58 +0700

    fix template communication user

--- src/views/CommunicationBoard/User/index.vue
+++ src/views/CommunicationBoard/User/index.vue
@@ -2,13 +2,13 @@
   <action-header title="기타소득자등록" @actionSave="false" :buttonSave="false" />
   <div class="page-content" id="announcement-user">
     <a-tabs v-model:activeKey="activeKey" type="card" class="tab-group mt-10">
-      <a-tab-pane :key="1" tab="보험사무대행 신청현황 관리" >
+      <a-tab-pane :key="1" tab="최신글" >
         <Tab1 @activeKey="actionActiveKey"/>
       </a-tab-pane>
-      <a-tab-pane :key="2" tab="사업장 신청현황 관리" >
+      <a-tab-pane :key="2" tab="알림" >
         <Tab2 />
       </a-tab-pane>
-      <a-tab-pane :key="3" tab="사원 신청현환 관리" >
+      <a-tab-pane :key="3" tab="문의" >
         <Tab3 />
       </a-tab-pane>
     </a-tabs>
@@ -38,21 +38,6 @@ export default defineComponent({
     }
   }
 });
-// import { DataRow, DataRowKey, OpenRowKey } from "@/views/CommunicationBoard/type";
-
-
-// const openRow = (data: DataRow) => {
-//   if (data.expressionType !== 3) {
-//     activeKey.value = '2'
-//   } else {
-//     activeKey.value = '3'
-//   }
-//   dataRow.value = data
-//   // console.log('%c data', 'color: red', data)
-// }
-
-// provide(OpenRowKey, openRow)
-// provide(DataRowKey, dataRow)
 </script>
 
 <style scoped></style>
