6b3d5bd523639eee8958d15682007ebd3a3eb10f
huntfpoly12 <125422305+huntfpoly12@users.noreply.github.com>
2023-07-05 13:36:06 +0700

    fix bf510 (#3717)
    Co-authored-by: Hieu Pham <hieunucek6@gmail.com>

--- src/views/BF/BF5/BF510/index.vue
+++ src/views/BF/BF5/BF510/index.vue
@@ -18,7 +18,7 @@
   <a-spin :spinning="loading">
     <DxDataGrid noDataText="내역이 없습니다" :show-row-lines="true" :hoverStateEnabled="true" :data-source="dataSource"
                 :allow-column-resizing="true" :show-borders="true" keyExpr="id" class="px-10" :loadPanel="false"
-                style="height: calc(100vh - 240px)">
+                style="height: calc(100vh - 200px)">
       <DxPaging :page-size="0"/>
       <DxExport :enabled="true"/>
       <DxSearchPanel :visible="true" placeholder="검색" :search-visible-columns="['facilityBusinessName']"/>
